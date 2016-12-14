2fcb4811b647190c0aac841394e1f2a707003f15
Kirill Müller <krlmlr+r@mailbox.org>
2016-12-14 13:51:18 +0100

    Vctr draft

--- inst/include/vctrs/vctr.h
+++ inst/include/vctrs/vctr.h
@@ -1,4 +1,18 @@
 #ifndef VCTRS_VCTRS_VCTR_H
 #define VCTRS_VCTRS_VCTR_H
 
+#include <vctrs/slicing_index.h>
+
+namespace vctrs {
+  class Vctr {
+  public:
+    virtual ~Vctr() {}
+
+  public:
+    virtual Vctr* get(size_t index) = 0;
+    virtual Vctr* subset(const SlicingIndex& index) = 0;
+    virtual Vctr* coerce_to(const Vctr& other) = 0;
+  };
+}
+
 #endif //VCTRS_VCTRS_VCTR_H
