c69b2fe52b4baaed6a4f7fe94ef5edc139dd1d9e
Kirill Müller <krlmlr+r@mailbox.org>
2016-12-14 15:17:39 +0100

    combine()

--- src/vctrs.cpp
+++ src/vctrs.cpp
@@ -1,2 +1,15 @@
 #include <Rcpp.h>
 #include <vctrs.h>
+
+
+using namespace vctrs;
+
+// [[Rcpp::export]]
+SEXP combine(SEXP x1, SEXP x2) {
+  std::auto_ptr<Vctr> v1(VctrFactory::create(x1));
+  VctrBuilder vb(*v1);
+  std::auto_ptr<Vctr> v2(VctrFactory::create(x2));
+  vb.append(*v2);
+
+  return vb.get_vctr().get_sexp();
+}
