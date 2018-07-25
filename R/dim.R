63c8fe82a5d3a6fbdd7de6491d49a3e7ec4be3b2
hadley <h.wickham@gmail.com>
2018-07-25 17:18:15 -0500

    Document dimension functions

--- R/dim.R
+++ R/dim.R
@@ -1,8 +1,30 @@
+#' Vector dimensions
+#'
+#' * `vec_length()` returns the length of the vector, which is the number of
+#'   rows if multidimensional
+#' * µ`vec_dims()` gives the dimensionality (i.e. number of dimensions)
+#' * `vec_dim()` returns the size of each dimension
+#'
+#' Unlike base R, we treat vectors with `NULL` dimensions as 1d. This
+#' simplifies the type system by eliding a special case. Compared to base R
+#' equivalents `vec_length()` acts like `NROW()`, and `vec_dim()` returns
+#' `length()`, not `NULL`, when `x` is 1d.
+#'
+#' @param x A vector
+#' @name dims
+#' @examples
+#' # Compared to base R
+#' x <- 1:5
+#' dim(x)
+#' vec_dim(x)
+#'
+#' y <- matrix(1:6, nrow = 2)
+#' length(y)
+#' vec_length(y)
+NULL
 
-# Unlike base R, no vector has `NULL` dimensions. `vec_length()` is
-# equivalent to `NROW()`, but has a name that is easily pronounced.
-# `vec_dims()` gives the dimensionality of a vector, i.e. the number
-# of dimensions
+#' @export
+#' @rdname dims
 vec_length <- function(x) {
   d <- dim(x)
   if (is.null(d)) {
@@ -12,10 +34,14 @@ vec_length <- function(x) {
   }
 }
 
+#' @export
+#' @rdname dims
 vec_dim <- function(x) {
   dim(x) %||% length(x)
 }
 
+#' @export
+#' @rdname dims
 vec_dims <- function(x) {
   d <- dim(x)
   if (is.null(d)) {
