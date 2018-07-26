587106c2ea32e83bf6ffbcdce1108db316b322a7
hadley <h.wickham@gmail.com>
2018-07-26 13:35:41 -0500

    Add coercions for factors and date/times

--- R/type-max.R
+++ R/type-max.R
@@ -6,6 +6,8 @@ vectype_max.NULL <- function(x, y, strict = TRUE) {
   vec_subset(y, 0L)
 }
 
+# Numeric-ish ----------------------------------------------------------
+
 vectype_max.logical <- function(x, y, strict = TRUE) {
   if (is_null(y)) {
     logical()
@@ -16,7 +18,7 @@ vectype_max.logical <- function(x, y, strict = TRUE) {
   } else if (is_bare_double(y)) {
     double()
   } else {
-    list_fallback(x, y, strict = strict)
+    fallback(list(), x, y, strict = strict)
   }
 }
 
@@ -30,7 +32,7 @@ vectype_max.integer <- function(x, y, strict = TRUE) {
   } else if (is_bare_double(y)) {
     double()
   } else {
-    list_fallback(x, y, strict = strict)
+    fallback(list(), x, y, strict = strict)
   }
 }
 
@@ -44,31 +46,76 @@ vectype_max.double <- function(x, y, strict = TRUE) {
   } else if (is_bare_double(y)) {
     double()
   } else {
-    list_fallback(x, y, strict = strict)
+    fallback(list(), x, y, strict = strict)
   }
 }
+
+
+# Characters and factors --------------------------------------------------
 
 vectype_max.character <- function(x, y, strict = TRUE) {
   if (is_null(y) || is_bare_character(y)) {
     character()
+  } else if (!strict && is.factor(y)) {
+    character()
   } else {
-    list_fallback(x, y, strict = strict)
+    fallback(list(), x, y, strict = strict)
   }
 }
+
+vectype_max.factor <- function(x, y, strict = TRUE) {
+  if (is_null(y)) {
+    vec_subset(x, 0L)
+  } else if (is.factor(y)) {
+    switch(
+      set_compare(levels(x), levels(y)),
+      equal = vec_subset(x, 0L),
+      x_in_y = vec_subset(y, 0L),
+      y_in_x = vec_subset(x, 0L),
+      fallback(character(), x, y, strict = strict) # needs custom error
+    )
+  } else if (!strict && is.character(y)) {
+    character()
+  } else {
+    fallback(list(), x, y, strict = TRUE)
+  }
+}
+
+# Date/times --------------------------------------------------------------
+
+vectype_max.Date <- function(x, y, strict = TRUE) {
+  if (is_null(y) || inherits(y, "Date")) {
+    x[0]
+  } else if (inherits(y, "POSIXt")) {
+    y[0]
+  } else {
+    fallback(list(), x, y, strict = strict)
+  }
+}
+
+vectype_max.POSIXt <- function(x, y, strict = TRUE) {
+  if (is_null(y) || inherits(y, "Date") || inherits(y, "POSIXt")) {
+    x[0]
+  } else {
+    fallback(list(), x, y, strict = strict)
+  }
+}
+
+# Lists -------------------------------------------------------------------
 
 vectype_max.list <- function(x, y, strict = TRUE) {
   if (is_null(y) || is_bare_list(y)) {
     list()
   } else {
-    list_fallback(x, y, strict = strict)
+    fallback(x, y, list(), strict = strict)
   }
 }
 
 # Helpers -----------------------------------------------------------------
 
-list_fallback <- function(x, y, strict = TRUE) {
+fallback <- function(fallback, x, y, strict = TRUE) {
   if (!strict) {
-    list()
+    fallback
   } else {
     # should be classed condition
     stop("No maximum type for ", vec_type(x), " and ", vec_type(y), call. = FALSE)
@@ -84,11 +131,11 @@ coerces_to <- function(x, y, ...) {
   })
 }
 
-maxtype_mat <- function(types) {
+maxtype_mat <- function(types, strict = TRUE) {
   names(types) <- map_chr(types, vec_type)
 
-  grid <- as_tibble(expand.grid(x = types, y = types))
-  grid$max <- map2_chr(grid$x, grid$y, coerces_to)
+  grid <- expand.grid(x = types, y = types)
+  grid$max <- map2_chr(grid$x, grid$y, coerces_to, strict = strict)
 
   matrix(
     grid$max,
--- R/utils.R
+++ R/utils.R
@@ -8,3 +8,15 @@ indent <- function(x, n) {
   out <- Map(gsub, "\n", paste0("\n", pad), x)
   unlist(out, use.names = FALSE)
 }
+
+set_compare <- function(x, y) {
+  if (identical(x, y) || setequal(x, y)) {
+    "equal"
+  } else if (length(setdiff(x, y)) == 0) {
+    "x_in_y"
+  } else if (length(setdiff(y, x)) == 0) {
+    "y_in_x"
+  } else {
+    "notequal"
+  }
+}
