14c44af66260a3b17641479b75662f9bfc18fa00
hadley <h.wickham@gmail.com>
2018-07-27 13:53:28 -0500

    Vector coercion should only preserve names

--- R/type-coerce.R
+++ R/type-coerce.R
@@ -5,12 +5,6 @@ vectype_coerce <- function(x, val) {
 
 # Base vectors --------------------------------------------------------------
 
-# Unexported wrapper around Rf_coerceVector()
-vec_coerce_bare <- function(x, type) {
-  coerce <- env_get(ns_env("rlang"), "vec_coerce")
-  coerce(x, type)
-}
-
 #' @export
 vectype_coerce.NULL <- function(x, val) {
   val
@@ -18,22 +12,22 @@ vectype_coerce.NULL <- function(x, val) {
 
 #' @export
 vectype_coerce.logical <- function(x, val) {
-  vec_coerce_bare(val, "logical")
+  set_names(as.logical(val), names(val))
 }
 
 #' @export
 vectype_coerce.integer <- function(x, val) {
-  vec_coerce_bare(val, "integer")
+  set_names(as.integer(val), names(val))
 }
 
 #' @export
 vectype_coerce.double <- function(x, val) {
-  vec_coerce_bare(val, "double")
+  set_names(as.double(val), names(val))
 }
 
 #' @export
 vectype_coerce.character <- function(x, val) {
-  vec_coerce_bare(val, "character")
+  set_names(as.character(val), names(val))
 }
 
 #' @export
