0fad50c3c20b525559220eaa0f09758b5f5c467a
hadley <h.wickham@gmail.com>
2018-07-29 16:49:11 -0500

    Add tests of basic vec_c functionality

--- tests/testthat/test-coerce.R
+++ tests/testthat/test-coerce.R
@@ -3,6 +3,26 @@ context("test-coerce")
 
 # vec_c -------------------------------------------------------------------
 
+test_that("zero length input returns NULL", {
+  expect_equal(vec_c(), NULL)
+  expect_equal(vec_c(NULL), NULL)
+})
+
+test_that("NULL is idempotent", {
+  expect_equal(vec_c(NULL, 1:10), 1:10)
+  expect_equal(vec_c(1:10, NULL), 1:10)
+})
+
 test_that("all inputs must be 1d", {
   expect_error(vec_c(mtcars))
+})
+
+test_that("different types are coerced to common", {
+  expect_equal(vec_c(TRUE, 1L, 1), c(1, 1, 1))
+  expect_equal(vec_c(TRUE, 2:4), 1:4)
+})
+
+test_that("setting strict = FALSE allows relaxes coercions", {
+  expect_warning(x <- vec_c(1:2, "x", .strict = FALSE), "Coercing")
+  expect_equal(x, list(1L, 2L, "x"))
 })
