111ba04eb6961cd1ddb1a1c867a140921ddaece7
hadley <h.wickham@gmail.com>
2018-08-06 11:14:12 -0500

    Move vec_c to own file

--- tests/testthat/test-coerce.R
+++ tests/testthat/test-coerce.R
@@ -17,36 +17,3 @@ test_that("empty input returns list()", {
 test_that("output has consistent type", {
   expect_equal(vec_coerce(TRUE, 1), list_of(1, 1))
 })
-
-# vec_c -------------------------------------------------------------------
-
-test_that("zero length input returns NULL", {
-  expect_equal(vec_c(), NULL)
-  expect_equal(vec_c(NULL), NULL)
-})
-
-test_that("NULL is idempotent", {
-  expect_equal(vec_c(NULL, 1:10), 1:10)
-  expect_equal(vec_c(1:10, NULL), 1:10)
-})
-
-test_that("all inputs must be 1d", {
-  expect_error(vec_c(mtcars))
-})
-
-test_that("different types are coerced to common", {
-  expect_equal(vec_c(TRUE, 1L, 1), c(1, 1, 1))
-  expect_equal(vec_c(TRUE, 2:4), 1:4)
-})
-
-test_that("specified .types allows more casts", {
-  expect_equal(vec_c(TRUE, .type = character()), "TRUE")
-})
-
-test_that("combines outer an inner names", {
-  expect_equal(vec_c(x = 1), c(x = 1))
-  expect_equal(vec_c(c(x = 1)), c(x = 1))
-
-  expect_equal(vec_c(c(x = 1:2)), c(x1 = 1, x2 = 2))
-  expect_equal(vec_c(y = c(x = 1)), c(y..x = 1))
-})
