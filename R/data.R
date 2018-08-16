39c8e90641e9f6541e07543e6241f1cb3b1b0503
hadley <h.wickham@gmail.com>
2018-08-16 14:08:20 -0500

    Document and export vec_data

--- R/data.R
+++ R/data.R
@@ -1,4 +1,14 @@
+#' Return the underlying data from a vector
+#'
+#' Currently, due to the underlying memory architecture of vectors, this
+#' requires a full copy of the underlying data.
+#'
+#' @param x A vector
+#' @export
+#' @return The data underlying `x`, free from an attributes.
 vec_data <- function(x) {
+  stopifnot(is_vector(x))
+
   # TODO: implement with ALTREP to avoid making a copy
   attributes(x) <- NULL
   x
