90a91eb7ed57c2f7658eb4cc6207bcfa27d5f0be
hadley <h.wickham@gmail.com>
2018-08-20 18:02:19 -0500

    Finish unknown tests

--- tests/testthat/test-unknown.R
+++ tests/testthat/test-unknown.R
@@ -20,3 +20,11 @@ test_that("unknown type is idempotent", {
   rhs <- map(types, vec_type2, y = unknown())
   expect_equal(types, rhs)
 })
+
+test_that("casting to/from unknown leaves as/is", {
+  expect_equal(vec_cast(1, unknown()), 1)
+})
+
+test_that("has useful print method", {
+  expect_known_output(unknown(), print = TRUE, file = test_path("test-unknown.txt"))
+})
