72f1fb03849afd060519ff8c6069aa495144fc8e
hadley <h.wickham@gmail.com>
2018-09-08 08:22:14 -0500

    Export vec_method_register
    Fixes #74

---
+++ R/register-s3.R
@@ -0,0 +1,56 @@
+#' Register a method for a suggested dependency
+#'
+#' Generally, the recommend way to register an S3 method is to use the
+#' `S3Method()` namespace directive (often generated automatically be the
+#' `@export` roxygen2 tag). However, this technique requires that the generic
+#' be in an imported package, and sometimes you want to suggest a package,
+#' and only provide a method when that package is loaded. `vec_method_dynamic()`
+#' should be called from your package's `.onLoad()` to dynamically register
+#' a method only if the generic's package is loaded.
+#'
+#' `vec_method_register()` is also useful when demonstrating class creation
+#' in a vignette, since as of R 3.5.0, method lookup no longer always involves
+#' the lexical scope.
+#'
+#' @param method_name Name of the method in the form `pkg::generic.class`.
+#' @param method Optionally, the implementation of the method. By default,
+#'   this will be found by looking for a function called `generic.class`
+#'   in the package environment.
+#' @export
+#' @examples
+#' # A typical use case is to dynamically register tibble/pillar methods
+#' # for your class. That way you avoid creating a hard depedency on packages
+#' # that are not essential, while still providing finer control over
+#' # printing when they are used.
+#'
+#' .onLoad <- function(...) {
+#'   vec_method_dynamic("pillar::pillar_shaft.vctrs_vctr")
+#'   vec_method_dynamic("tibble::type_sum.vctrs_vctr")
+#' }
+#' @keywords internal
+vec_method_register <- function(method_name, method = NULL) {
+  stopifnot(is.character(method_name), length(method_name) == 1)
+
+  pieces <- strsplit(method_name, "::|[.]")[[1]]
+  stopifnot(length(pieces) == 3)
+  package <- pieces[[1]]
+  generic <- pieces[[2]]
+  class <- pieces[[3]]
+
+  if (is.null(method)) {
+    method <- get(paste0(generic, ".", class), envir = parent.frame())
+  }
+  stopifnot(is.function(method))
+
+  if (package %in% loadedNamespaces()) {
+    registerS3method(generic, class, method, envir = asNamespace(package))
+  }
+
+  # Always register hook in case package is later unloaded & reloaded
+  setHook(
+    packageEvent(package, "onLoad"),
+    function(...) {
+      registerS3method(generic, class, method, envir = asNamespace(package))
+    }
+  )
+}
--- R/zzz.R
+++ R/zzz.R
@@ -2,31 +2,8 @@
 .onLoad <- function(libname, pkgname) {
   backports::import(pkgname, "strrep")
 
-  register_s3_method("pillar", "pillar_shaft", "vctrs_vctr")
-  register_s3_method("tibble", "type_sum", "vctrs_vctr")
+  vec_method_register("pillar::pillar_shaft.vctrs_vctr")
+  vec_method_register("tibble::type_sum.vctrs_vctr")
 }
 
-register_s3_method <- function(pkg, generic, class, fun = NULL) {
-  stopifnot(is.character(pkg), length(pkg) == 1)
-  stopifnot(is.character(generic), length(generic) == 1)
-  stopifnot(is.character(class), length(class) == 1)
-
-  if (is.null(fun)) {
-    fun <- get(paste0(generic, ".", class), envir = parent.frame())
-  } else {
-    stopifnot(is.function(fun))
-  }
-
-  if (pkg %in% loadedNamespaces()) {
-    registerS3method(generic, class, fun, envir = asNamespace(pkg))
-  }
-
-  # Always register hook in case package is later unloaded & reloaded
-  setHook(
-    packageEvent(pkg, "onLoad"),
-    function(...) {
-      registerS3method(generic, class, fun, envir = asNamespace(pkg))
-    }
-  )
-}
 # nocov end
