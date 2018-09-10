40aa6e3951fc14dd19ced0ed3663b70688838dad
hadley <h.wickham@gmail.com>
2018-09-10 13:17:54 -0500

    Refactor error/warning generation

--- R/conditions.R
+++ R/conditions.R
@@ -9,81 +9,86 @@
 #' @param subclass Use if you want to further customise the class
 #' @param locations For `warn_lossy_cast()`, an optional vector giving the
 #'   locations where `x` lost information.
+#' @param ...,message,.subclass Only use these fields when creating a subclass.
 #' @name vctrs-conditions
 NULL
 
-stop_incompatible <- function(x, y, details = NULL, subclass = NULL) {
+stop_incompatible <- function(x, y, details = NULL, ..., message = NULL, .subclass = NULL) {
+  if (is.null(message)) {
+    message <- glue::glue("Incompatible inputs: {vec_ptype_full(x)} and {vec_ptype_full(y)}")
+    if (!is.null(details)) {
+      message <- paste0(message, "\n", details)
+    }
+  }
+
   abort(
-    c(subclass, "error_incompatible"),
-    message = "Incompatible types",
+    message,
+    .subclass = c(.subclass, "error_incompatible"),
     x = x,
     y = y,
-    details = details
+    details = details,
+    ...
   )
 }
 
 #' @rdname vctrs-conditions
 #' @export
-stop_incompatible_type <- function(x, y, details = NULL) {
+stop_incompatible_type <- function(x, y, details = NULL, ..., message = NULL, .subclass = NULL) {
+  message <- message %||% glue_lines(
+    "No common type for {vec_ptype_full(x)} and {vec_ptype_full(y)}",
+    details
+  )
+
   stop_incompatible(
     x, y,
+    message = message,
     details = details,
-    subclass = "error_incompatible_type"
+    ...,
+    .subclass = c(.subclass, "error_incompatible_type")
   )
 }
 
 #' @rdname vctrs-conditions
 #' @export
-stop_incompatible_cast <- function(x, y, details = NULL) {
+stop_incompatible_cast <- function(x, y, details = NULL, ..., message = NULL, .subclass = NULL) {
+
+  message <- message %||% glue_lines(
+    "Can't cast {vec_ptype_full(x)} to {vec_ptype_full(y)}",
+    details
+  )
+
   stop_incompatible(
     x, y,
+    message = message,
     details = details,
-    subclass = "error_incompatible_cast"
+    ...,
+    .subclass = c(.subclass, "error_incompatible_cast")
   )
 }
 
 #' @rdname vctrs-conditions
 #' @export
-warn_lossy_cast <- function(x, y, locations = NULL, details = NULL) {
+warn_lossy_cast <- function(x, y, locations = NULL, details = NULL, ..., message = NULL, .subclass = NULL) {
+
+  message <- message %||% glue_lines(
+    "Lossy cast from {vec_ptype_full(x)} to {vec_ptype_full(y)}",
+    inline_list("Locations: ", locations),
+    details
+  )
+
   warn(
-    "warning_lossy_cast",
-    message = "Lossy cast",
+    message,
     x = x,
     y = y,
     locations = locations,
-    details = details
+    details = details,
+    ...,
+    .subclass = c("warning_lossy_cast", .subclass),
   )
 }
 
-#' @export
-conditionMessage.error_incompatible_type <- function(c) {
-  msg <- glue::glue_data(c, "No common type for {vec_ptype_full(x)} and {vec_ptype_full(y)}")
-  if (!is.null(c$details)) {
-    msg <- paste0(msg, "\n", c$details)
-  }
-  msg
-}
-
-#' @export
-conditionMessage.error_incompatible_cast <- function(c) {
-  msg <- glue::glue_data(c, "Can't cast {vec_ptype_full(x)} to {vec_ptype_full(y)}")
-  if (!is.null(c$details)) {
-    msg <- paste0(msg, "\n", c$details)
-  }
-  msg
-}
-
-#' @export
-conditionMessage.warning_lossy_cast <- function(c) {
-  msg <- glue::glue_data(c, "Lossy cast from {vec_ptype_full(x)} to {vec_ptype_full(y)}")
-
-  if (!is.null(c$locations)) {
-    msg <- paste0(msg, "\n", inline_list("Locations: ", c$locations))
-  }
-
-  if (!is.null(c$details)) {
-    msg <- paste0(msg, "\n", c$details)
-  }
-  msg
-
+glue_lines <- function(..., env = parent.frame()) {
+  lines <- c(...)
+  out <- map_chr(lines, glue::glue, .envir = env)
+  paste(out, collapse = "\n")
 }
