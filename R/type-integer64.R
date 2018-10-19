1a6a9d53c035581deb8b40e9db2e65e0c8f0e6e4
Romain Francois <romain@purrple.cat>
2018-10-19 15:30:32 +0200

    +new_int64, re-org files

---
+++ R/type-integer64.R
@@ -0,0 +1,135 @@
+
+#' 64 bit integers
+#'
+#' A [integer64] vector is 64 bits integer vector. Details
+#' are implented in the `bit64` package.
+#'
+#' These functions help the `integer64` class from `bit64` in to
+#' the vctrs type system by providing constructors, coercion functions,
+#' and casting functions. `new_int64()` is a low-level
+#' constructor that only checks the type is valid, so
+#' is for expert use only.
+#'
+#' @param x 64 bit integer vector
+#' @param ...,class Used to for subclasses.
+#' @keywords internal
+#' @export
+new_int64 <- function(x = bit64::integer64(), ..., class = character()) {
+  stopifnot(inherits(x, "integer64"))
+  x
+}
+
+# Print -------------------------------------------------------------------
+
+#' @rdname new_int64
+#' @export
+vec_ptype_full.integer64 <- function(x) {
+  "integer64"
+}
+
+#' @rdname new_int64
+#' @export
+vec_ptype_abbr.integer64 <- function(x) {
+  "int64"
+}
+
+
+# Coerce ------------------------------------------------------------------
+
+#' @export
+#' @rdname new_int64
+#' @export vec_type2.integer64
+#' @method vec_type2 integer64
+vec_type2.integer64 <- function(x, y) {
+  UseMethod("vec_type2.integer64", y)
+}
+
+#' @method vec_type2.integer64 default
+#' @export
+vec_type2.integer64.default <- function(x, y) stop_incompatible_type(x, y)
+
+#' @method vec_type2.integer64 integer64
+#' @export
+vec_type2.integer64.integer64 <- function(x, y) bit64::integer64()
+
+
+#' @method vec_type2.integer64 integer
+#' @export
+vec_type2.integer64.integer <- function(x, y) bit64::integer64()
+
+#' @method vec_type2.integer integer64
+#' @export
+vec_type2.integer.integer64 <- function(x, y) bit64::integer64()
+
+#' @method vec_type2.integer64 logical
+#' @export
+vec_type2.integer64.logical <- function(x, y) bit64::integer64()
+
+#' @method vec_type2.logical integer64
+#' @export
+vec_type2.logical.integer64 <- function(x, y) bit64::integer64()
+
+
+# Cast --------------------------------------------------------------------
+
+#' @export
+#' @rdname new_int64
+#' @export vec_cast.integer64
+#' @method vec_cast integer64
+vec_cast.integer64 <- function(x, to) UseMethod("vec_cast.integer64")
+
+#' @export
+#' @method vec_cast.integer64 default
+vec_cast.integer64.default <- function(x, to) stop_incompatible_cast(x, to)
+
+#' @export
+#' @method vec_cast.integer64 integer64
+vec_cast.integer64.integer64 <- function(x, to) x
+
+#' @export
+#' @method vec_cast.integer64 integer
+vec_cast.integer64.integer <- function(x, to) {
+  bit64::as.integer64(x)
+}
+
+#' @export
+#' @method vec_cast.integer integer64
+vec_cast.integer.integer64 <- function(x, to) {
+  as.integer(x)
+}
+
+#' @export
+#' @method vec_cast.integer64 logical
+vec_cast.integer64.logical <- function(x, to) {
+  bit64::as.integer64(x)
+}
+
+#' @export
+#' @method vec_cast.logical integer64
+vec_cast.logical.integer64 <- function(x, to) {
+  as.logical(x)
+}
+
+#' @export
+#' @method vec_cast.integer64 character
+vec_cast.integer64.character <- function(x, to) {
+  bit64::as.integer64(x)
+}
+
+#' @export
+#' @method vec_cast.character integer64
+vec_cast.character.integer64 <- function(x, to) {
+  as.character(x)
+}
+
+#' @export
+#' @method vec_cast.integer64 double
+vec_cast.integer64.double <- function(x, to) {
+  bit64::as.integer64(x)
+}
+
+#' @export
+#' @method vec_cast.logical integer64
+vec_cast.double.integer64 <- function(x, to) {
+  as.double(x)
+}
--- tests/testthat/test-bit64.R
+++ tests/testthat/test-bit64.R
@@ -1,20 +1,29 @@
-context("test-bit64")
+context("test-type-bit64")
+
+test_that("new_int64 works", {
+  x <- new_int64()
+  expect_is(x, "integer64")
+  expect_error(new_int64(1:10))
+})
 
 test_that("casting of integer64 works", {
   x <- bit64::as.integer64(1:10)
   expect_equal(vec_cast(x, bit64::integer64()), x)
 
   expect_equal(vec_cast(x, integer()), 1:10)
-  expect_equal(vec_cast(x, double()), as.numeric(1:10))
-  expect_equal(vec_cast(x, character()), as.character(1:10))
-  expect_equal(vec_cast(x, logical()), rep(TRUE, 10L))
-  expect_error(vec_cast(x, factor()))
-
   expect_equal(vec_cast(1:10, bit64::integer64()), x)
+
+  expect_equal(vec_cast(x, double()), as.double(x))
   expect_equal(vec_cast(as.numeric(1:10), bit64::integer64()), x)
+
+  expect_equal(vec_cast(x, character()), as.character(x))
   expect_equal(vec_cast(as.character(1:10), bit64::integer64()), x)
+
+  expect_equal(vec_cast(x, logical()), rep(TRUE, 10L))
   expect_equal(vec_cast(c(TRUE, FALSE), bit64::integer64()), bit64::as.integer64(c(1, 0)))
-  expect_equal(vec_cast(factor(c("a", "b")), bit64::integer64()), bit64::as.integer64(c(1, 2)))
+
+  expect_error(vec_cast(x, factor()))
+  expect_error(vec_cast(factor(c("a", "b"))))
 })
 
 test_that("vec_type2 for integer64 works", {
@@ -24,14 +33,14 @@ test_that("vec_type2 for integer64 works", {
   expect_equal(vec_type2(x, 1L), bit64::integer64())
   expect_equal(vec_type2(1L, x), bit64::integer64())
 
-  expect_equal(vec_type2(x, 1), bit64::integer64())
-  expect_equal(vec_type2(1, x), bit64::integer64())
-
-  expect_equal(vec_type2(x, ""), character())
-  expect_equal(vec_type2("", x), character())
-
   expect_equal(vec_type2(x, TRUE), bit64::integer64())
   expect_equal(vec_type2(TRUE, x), bit64::integer64())
+
+  expect_error(vec_type2(x, 1))
+  expect_error(vec_type2(1, x))
+
+  expect_error(vec_type2(x, ""))
+  expect_error(vec_type2("", x))
 
   expect_error(vec_type2(data.frame(), x))
   expect_error(vec_type2(x, data.frame()))
@@ -39,4 +48,5 @@ test_that("vec_type2 for integer64 works", {
 
 test_that("vec_ptype_abbr.integer64", {
   expect_equal(vec_ptype_abbr(bit64::as.integer64(1:10)), "int64")
+  expect_equal(vec_ptype_full(bit64::as.integer64(1:10)), "integer64")
 })
