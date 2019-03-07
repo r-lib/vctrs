05dd79df7274173fe7df4bd925bf240e122e4be4
Lionel Henry <lionel.hry@gmail.com>
2019-03-07 14:59:42 +0100

    Extract `has_ptype()` and `has_size()`

--- R/assert.R
+++ R/assert.R
@@ -30,55 +30,45 @@
 #' @export
 vec_assert <- function(x, ptype = NULL, size = NULL, arg = NULL) {
   arg <- arg %||% as_label(substitute(x))
-  vec_is_impl(x, arg, ptype, size, assert = TRUE)
+
+  if (!has_ptype(x, ptype)) {
+    ptype <- vec_type(ptype)
+    x_type <- vec_type(x)
+    msg <- paste0("`", arg, "` must be <", vec_ptype_abbr(ptype), ">, not <", vec_ptype_abbr(x_type), ">.")
+    abort(
+      msg,
+      .subclass = c("vctrs_error_assert_ptype", "vctrs_error_assert"),
+      required = ptype,
+      actual = x_type
+    )
+  }
+
+  if (!has_size(x, size)) {
+    size <- vec_recycle(vec_cast(size, integer()), 1L)
+    x_size <- vec_size(x)
+    msg <- paste0("`", arg, "` must have size ", size, ", not size ", x_size, ".")
+    abort(
+      msg,
+      .subclass = c("vctrs_error_assert_size", "vctrs_error_assert"),
+      required = size,
+      actual = x_size
+    )
+  }
+
+  invisible(x)
 }
 #' @export
 vec_is <- function(x, ptype = NULL, size = NULL) {
-  vec_is_impl(x, "", ptype, size)
+  has_ptype(x, ptype) && has_size(x, size)
 }
 
-vec_is_impl <- function(x, arg, ptype = NULL, size = NULL, assert = FALSE) {
-  if (!is.null(ptype)) {
-    x_type <- vec_type(x)
-    ptype <- vec_type(ptype)
-
-    if (!identical(ptype, x_type)) {
-      if (assert) {
-        msg <- paste0("`", arg, "` must be <", vec_ptype_abbr(ptype), ">, not <", vec_ptype_abbr(x_type), ">.")
-        abort(
-          msg,
-          .subclass = c("vctrs_error_assert_ptype", "vctrs_error_assert"),
-          required = ptype,
-          actual = x_type
-        )
-      } else {
-        return(FALSE)
-      }
-    }
-  }
-
-  if (!is.null(size)) {
-    size <- vec_recycle(vec_cast(size, integer()), 1L)
-    x_size <- vec_size(x)
-
-    if (!identical(size, x_size)) {
-      if (assert) {
-        msg <- paste0("`", arg, "` must have size ", size, ", not size ", x_size, ".")
-        abort(
-          msg,
-          .subclass = c("vctrs_error_assert_size", "vctrs_error_assert"),
-          required = size,
-          actual = x_size
-        )
-      } else {
-        return(FALSE)
-      }
-    }
-  }
-
-  if (assert) {
-    invisible(x)
-  } else {
-    TRUE
-  }
+has_ptype <- function(x, ptype = NULL) {
+  ptype <- vec_type(ptype)
+  x_type <- vec_type(x)
+  is_null(ptype) || identical(ptype, x_type)
+}
+has_size <- function(x, size = NULL) {
+  size <- vec_recycle(vec_cast(size, integer()), 1L)
+  x_size <- vec_size(x)
+  is_null(size) || identical(size, x_size)
 }
