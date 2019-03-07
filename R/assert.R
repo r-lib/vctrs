810aed54de492aa25b4b770425caf32ce47899d2
Lionel Henry <lionel.hry@gmail.com>
2019-03-07 15:17:29 +0100

    Separate `vec_is()` and `vec_assert()` implementations

--- R/assert.R
+++ R/assert.R
@@ -31,44 +31,56 @@
 vec_assert <- function(x, ptype = NULL, size = NULL, arg = NULL) {
   arg <- arg %||% as_label(substitute(x))
 
-  if (!has_ptype(x, ptype)) {
+  if (!is_null(ptype)) {
     ptype <- vec_type(ptype)
     x_type <- vec_type(x)
-    msg <- paste0("`", arg, "` must be <", vec_ptype_abbr(ptype), ">, not <", vec_ptype_abbr(x_type), ">.")
-    abort(
-      msg,
-      .subclass = c("vctrs_error_assert_ptype", "vctrs_error_assert"),
-      required = ptype,
-      actual = x_type
-    )
+    if (!identical(x_type, ptype)) {
+      ptype <- vec_type(ptype)
+      x_type <- vec_type(x)
+      msg <- paste0("`", arg, "` must be <", vec_ptype_abbr(ptype), ">, not <", vec_ptype_abbr(x_type), ">.")
+      abort(
+        msg,
+        .subclass = c("vctrs_error_assert_ptype", "vctrs_error_assert"),
+        required = ptype,
+        actual = x_type
+      )
+    }
   }
 
-  if (!has_size(x, size)) {
+  if (!is_null(size)) {
     size <- vec_recycle(vec_cast(size, integer()), 1L)
     x_size <- vec_size(x)
-    msg <- paste0("`", arg, "` must have size ", size, ", not size ", x_size, ".")
-    abort(
-      msg,
-      .subclass = c("vctrs_error_assert_size", "vctrs_error_assert"),
-      required = size,
-      actual = x_size
-    )
+    if (!identical(x_size, size)) {
+      msg <- paste0("`", arg, "` must have size ", size, ", not size ", x_size, ".")
+      abort(
+        msg,
+        .subclass = c("vctrs_error_assert_size", "vctrs_error_assert"),
+        required = size,
+        actual = x_size
+      )
+    }
   }
 
   invisible(x)
 }
+#' @rdname vec_assert
 #' @export
 vec_is <- function(x, ptype = NULL, size = NULL) {
-  has_ptype(x, ptype) && has_size(x, size)
-}
+  if (!is_null(ptype)) {
+    ptype <- vec_type(ptype)
+    x_type <- vec_type(x)
+    if (!identical(x_type, ptype)) {
+      return(FALSE)
+    }
+  }
 
-has_ptype <- function(x, ptype = NULL) {
-  ptype <- vec_type(ptype)
-  x_type <- vec_type(x)
-  is_null(ptype) || identical(ptype, x_type)
-}
-has_size <- function(x, size = NULL) {
-  size <- vec_recycle(vec_cast(size, integer()), 1L)
-  x_size <- vec_size(x)
-  is_null(size) || identical(size, x_size)
+  if (!is_null(size)) {
+    size <- vec_recycle(vec_cast(size, integer()), 1L)
+    x_size <- vec_size(x)
+    if (!identical(x_size, size)) {
+      return(FALSE)
+    }
+  }
+
+  TRUE
 }
