0bc990b58d63f96b0a4c399690ea336eb7a75c22
Lionel Henry <lionel.hry@gmail.com>
2019-03-27 15:25:52 +0100

    Temporary fix for backtraces in vignettes

--- R/vctrs-package.R
+++ R/vctrs-package.R
@@ -2,3 +2,12 @@
 #' @import rlang
 #' @useDynLib vctrs, .registration = TRUE
 "_PACKAGE"
+
+
+# FIXME: Fix for backtraces in vignettes. Remove once rlang 0.3.3 is out.
+#' @export
+as.character.rlang_error <- function(x, ...) {
+  # Don't generate backtrace or reminder in programmatic uses. Fixes
+  # backtraces in knitr.
+  x$message
+}
