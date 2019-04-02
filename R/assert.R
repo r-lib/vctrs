04ba8505a6876a3587d4c4b25af789861dc00711
Lionel Henry <lionel.hry@gmail.com>
2019-04-02 09:37:03 +0200

    Generate assertion label lazily
    Part of #263

--- R/assert.R
+++ R/assert.R
@@ -31,9 +31,7 @@
 #'   throws a typed error (see section on error types) or returns `x`,
 #'   invisibly.
 #' @export
-vec_assert <- function(x, ptype = NULL, size = NULL, arg = NULL) {
-  arg <- arg %||% as_label(substitute(x))
-
+vec_assert <- function(x, ptype = NULL, size = NULL, arg = as_label(substitute(x))) {
   if (!vec_is_vector(x)) {
     msg <- glue::glue("`{ arg }` must be a vector, not { friendly_type_of(x) }")
     abort(msg, "vctrs_error_assert_scalar", actual = x)
