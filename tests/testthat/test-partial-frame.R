83ca129481c4b708b0e6ce51c1fc8c7a8314295c
Lionel Henry <lionel.hry@gmail.com>
2019-04-04 17:50:57 +0200

    Fix assertion of partial types (#275)
    Closes #198

--- tests/testthat/test-partial-frame.R
+++ tests/testthat/test-partial-frame.R
@@ -24,3 +24,25 @@ test_that("partial variables added to end if not in data", {
   expect_named(vec_type_common(pf, df), c("x", "y"))
   expect_named(vec_type_common(df, pf), c("x", "y"))
 })
+
+test_that("can assert partial frames based on column presence", {
+  pf <- partial_frame(y = 1)
+
+  expect_true(vec_is(data.frame(y = 2), pf))
+  expect_false(vec_is(data.frame(x = 1), pf))
+
+  expect_true(vec_is(data.frame(x = 1, y = 2), pf))
+  expect_true(vec_is(data.frame(x = 1, y = 2, z = 3), pf))
+
+  pf <- partial_frame(y = 1, z = 3)
+
+  expect_false(vec_is(data.frame(y = 2), pf))
+  expect_false(vec_is(data.frame(x = 1), pf))
+  expect_false(vec_is(data.frame(x = 1, y = 2), pf))
+  expect_true(vec_is(data.frame(x = 1, y = 2, z = 3), pf))
+})
+
+test_that("can assert partial frames based on column type", {
+  pf <- partial_frame(y = 1)
+  expect_false(vec_is(data.frame(y = "2"), pf))
+})
