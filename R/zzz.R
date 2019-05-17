12004573a8714e6ddc3a37201149ed2e016f0901
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-17 11:40:09 +0200

    Don't register pillar methods

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,6 @@
 .onLoad <- function(libname, pkgname) {
   backports::import(pkgname, "strrep")
 
-  s3_register("pillar::pillar_shaft", "vctrs_vctr")
-  s3_register("pillar::pillar_shaft", "vctrs_list_of")
-  s3_register("pillar::type_sum", "vctrs_vctr")
-  s3_register("pillar::type_sum", "vctrs_unspecified")
   s3_register("generics::as.factor", "vctrs_vctr")
   s3_register("generics::as.ordered", "vctrs_vctr")
   s3_register("generics::as.difftime", "vctrs_vctr")
