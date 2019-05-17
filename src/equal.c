0364eda54378c246911f55862a109913d02e9f36
Lionel Henry <lionel.hry@gmail.com>
2019-05-17 09:49:13 +0200

    Reorganise `equal.c`

--- src/equal.c
+++ src/equal.c
@@ -1,9 +1,103 @@
 #include <math.h>
 #include "vctrs.h"
 
+static int lgl_equal_scalar(int* x, int* y, bool na_equal);
+static int int_equal_scalar(int* x, int* y, bool na_equal);
+static int dbl_equal_scalar(double* x, double* y, bool na_equal);
+static int chr_equal_scalar(SEXP* x, SEXP* y, bool na_equal);
+static int list_equal_scalar(SEXP x, R_len_t i, SEXP y, R_len_t j, bool na_equal);
+static int df_equal_scalar(SEXP x, R_len_t i, SEXP y, R_len_t j, bool na_equal);
+
+
+// [[ include("vctrs.h") ]]
+int equal_scalar(SEXP x, R_len_t i, SEXP y, R_len_t j, bool na_equal) {
+  switch (TYPEOF(x)) {
+  case LGLSXP: return lgl_equal_scalar(LOGICAL(x) + i, LOGICAL(y) + j, na_equal);
+  case INTSXP: return int_equal_scalar(INTEGER(x) + i, INTEGER(y) + j, na_equal);
+  case REALSXP: return dbl_equal_scalar(REAL(x) + i, REAL(y) + j, na_equal);
+  case STRSXP: return chr_equal_scalar(STRING_PTR(x) + i, STRING_PTR(y) + j, na_equal);
+  default: break;
+  }
+
+  switch (vec_typeof(x)) {
+  case vctrs_type_list: return list_equal_scalar(x, i, y, j, na_equal);
+  case vctrs_type_dataframe: return df_equal_scalar(x, i, y, j, na_equal);
+  default: break;
+  }
+
+  vctrs_stop_unsupported_type(vec_typeof(x), "equal_scalar()");
+}
+
+// [[ register() ]]
+SEXP vctrs_equal(SEXP x, SEXP y, SEXP na_equal_) {
+  enum vctrs_type type = vec_typeof(x);
+  if (type != vec_typeof(y) || vec_size(x) != vec_size(y)) {
+    Rf_errorcall(R_NilValue, "`x` and `y` must have same types and lengths");
+  }
+
+  bool na_equal = Rf_asLogical(na_equal_);
+
+  R_len_t n = vec_size(x);
+  SEXP out = PROTECT(Rf_allocVector(LGLSXP, n));
+  int32_t* p_out = LOGICAL(out);
+
+  switch (type) {
+  case vctrs_type_logical: {
+    int* x_ptr = LOGICAL(x);
+    int* y_ptr = LOGICAL(y);
+    for (R_len_t i = 0; i < n; ++i, ++x_ptr, ++y_ptr) {
+      p_out[i] = lgl_equal_scalar(x_ptr, y_ptr, na_equal);
+    }
+  }
+  case vctrs_type_integer: {
+    int* x_ptr = INTEGER(x);
+    int* y_ptr = INTEGER(y);
+    for (R_len_t i = 0; i < n; ++i, ++x_ptr, ++y_ptr) {
+      p_out[i] = int_equal_scalar(x_ptr, y_ptr, na_equal);
+    }
+    break;
+  }
+  case vctrs_type_double: {
+    double* x_ptr = REAL(x);
+    double* y_ptr = REAL(y);
+    for (R_len_t i = 0; i < n; ++i, ++x_ptr, ++y_ptr) {
+      p_out[i] = dbl_equal_scalar(x_ptr, y_ptr, na_equal);
+    }
+    break;
+  }
+  case vctrs_type_character: {
+    SEXP* x_ptr = STRING_PTR(x);
+    SEXP* y_ptr = STRING_PTR(y);
+    for (R_len_t i = 0; i < n; ++i, ++x_ptr, ++y_ptr) {
+      p_out[i] = chr_equal_scalar(x_ptr, y_ptr, na_equal);
+    }
+    break;
+  }
+  case vctrs_type_list: {
+    for (R_len_t i = 0; i < n; ++i) {
+      p_out[i] = list_equal_scalar(x, i, y, i, na_equal);
+    }
+    break;
+  }
+  case vctrs_type_dataframe: {
+    for (R_len_t i = 0; i < n; ++i) {
+      p_out[i] = df_equal_scalar(x, i, y, i, na_equal);
+    }
+    break;
+  }
+  case vctrs_type_scalar:
+    Rf_errorcall(R_NilValue, "Can't compare scalars with `vctrs_equal()`");
+  default:
+    Rf_error("Unimplemented type in `vctrs_equal()`");
+  }
+
+  UNPROTECT(1);
+  return out;
+}
+
+
 // Storing pointed values on the stack helps performance for the
 // `!na_equal` cases
-
 static int lgl_equal_scalar(int* x, int* y, bool na_equal) {
   int xi = *x;
   int yj = *y;
@@ -79,66 +173,9 @@ static int df_equal_scalar(SEXP x, R_len_t i, SEXP y, R_len_t j, bool na_equal) 
   return true;
 }
 
-// [[ include("vctrs.h") ]]
-int equal_scalar(SEXP x, R_len_t i, SEXP y, R_len_t j, bool na_equal) {
-  switch (TYPEOF(x)) {
-  case LGLSXP: return lgl_equal_scalar(LOGICAL(x) + i, LOGICAL(y) + j, na_equal);
-  case INTSXP: return int_equal_scalar(INTEGER(x) + i, INTEGER(y) + j, na_equal);
-  case REALSXP: return dbl_equal_scalar(REAL(x) + i, REAL(y) + j, na_equal);
-  case STRSXP: return chr_equal_scalar(STRING_PTR(x) + i, STRING_PTR(y) + j, na_equal);
-  default: break;
-  }
 
-  switch (vec_typeof(x)) {
-  case vctrs_type_list: return list_equal_scalar(x, i, y, j, na_equal);
-  case vctrs_type_dataframe: return df_equal_scalar(x, i, y, j, na_equal);
-  default: break;
-  }
-
-  vctrs_stop_unsupported_type(vec_typeof(x), "equal_scalar()");
-}
-
-// TODO: Sort attributes by tag before comparison
-
-// We don't propagate missingness from attributes because any missing
-// values in there are probably actual data
-static inline bool obj_equal_attrib(SEXP x, SEXP y) {
-  return equal_object(ATTRIB(x), ATTRIB(y), true);
-}
-
-// Same as `obj_` variant but propagates NA only for names
-static inline int vec_equal_attrib(SEXP x, SEXP y, bool na_equal) {
-  SEXP x_attrs = ATTRIB(x);
-  SEXP y_attrs = ATTRIB(y);
-
-  while (x_attrs != R_NilValue) {
-    if (y_attrs == R_NilValue) {
-      return false;
-    }
-
-    SEXP x_tag = TAG(x_attrs);
-    SEXP y_tag = TAG(x_attrs);
-
-    if (x_tag != y_tag) {
-      return false;
-    }
-
-    int eq;
-    if (x_tag == R_NamesSymbol) {
-      eq = equal_object(CAR(x_attrs), CAR(y_attrs), na_equal);
-    } else {
-      eq = equal_object(CAR(x_attrs), CAR(y_attrs), true);
-    }
-    if (eq <= 0) {
-      return(eq);
-    }
-
-    x_attrs = CDR(x_attrs);
-    y_attrs = CDR(y_attrs);
-  }
-
-  return true;
-}
+static inline bool obj_equal_attrib(SEXP x, SEXP y);
+static inline int vec_equal_attrib(SEXP x, SEXP y, bool na_equal);
 
 // [[ include("vctrs.h") ]]
 int equal_object(SEXP x, SEXP y, bool na_equal) {
@@ -244,6 +281,63 @@ int equal_object(SEXP x, SEXP y, bool na_equal) {
   return true;
 }
 
+// [[ register() ]]
+SEXP vctrs_equal_object(SEXP x, SEXP y, SEXP na_equal) {
+  return Rf_ScalarLogical(equal_object(x, y, Rf_asLogical(na_equal)));
+}
+
+// TODO: Sort attributes by tag before comparison
+
+// We don't propagate missingness from attributes because any missing
+// values in there are probably actual data
+static inline bool obj_equal_attrib(SEXP x, SEXP y) {
+  return equal_object(ATTRIB(x), ATTRIB(y), true);
+}
+
+// Same as `obj_` variant but propagates NA only for names
+static inline int vec_equal_attrib(SEXP x, SEXP y, bool na_equal) {
+  SEXP x_attrs = ATTRIB(x);
+  SEXP y_attrs = ATTRIB(y);
+
+  while (x_attrs != R_NilValue) {
+    if (y_attrs == R_NilValue) {
+      return false;
+    }
+
+    SEXP x_tag = TAG(x_attrs);
+    SEXP y_tag = TAG(x_attrs);
+
+    if (x_tag != y_tag) {
+      return false;
+    }
+
+    int eq;
+    if (x_tag == R_NamesSymbol) {
+      eq = equal_object(CAR(x_attrs), CAR(y_attrs), na_equal);
+    } else {
+      eq = equal_object(CAR(x_attrs), CAR(y_attrs), true);
+    }
+    if (eq <= 0) {
+      return(eq);
+    }
+
+    x_attrs = CDR(x_attrs);
+    y_attrs = CDR(y_attrs);
+  }
+
+  return true;
+}
+
+
+// [[ include("vctrs.h") ]]
+bool equal_names(SEXP x, SEXP y) {
+  SEXP x_names = Rf_getAttrib(x, R_NamesSymbol);
+  SEXP y_names = Rf_getAttrib(y, R_NamesSymbol);
+
+  return equal_object(x_names, y_names, true);
+}
+
+
 static bool equal_na(SEXP x, int i) {
   switch(TYPEOF(x)) {
   case LGLSXP:
@@ -273,82 +367,6 @@ static bool equal_na(SEXP x, int i) {
   }
 }
 
-// [[ include("vctrs.h") ]]
-bool equal_names(SEXP x, SEXP y) {
-  SEXP x_names = Rf_getAttrib(x, R_NamesSymbol);
-  SEXP y_names = Rf_getAttrib(y, R_NamesSymbol);
-
-  return equal_object(x_names, y_names, true);
-}
-
-
-// [[ register() ]]
-SEXP vctrs_equal(SEXP x, SEXP y, SEXP na_equal_) {
-  enum vctrs_type type = vec_typeof(x);
-  if (type != vec_typeof(y) || vec_size(x) != vec_size(y)) {
-    Rf_errorcall(R_NilValue, "`x` and `y` must have same types and lengths");
-  }
-
-  bool na_equal = Rf_asLogical(na_equal_);
-
-  R_len_t n = vec_size(x);
-  SEXP out = PROTECT(Rf_allocVector(LGLSXP, n));
-  int32_t* p_out = LOGICAL(out);
-
-  switch (type) {
-  case vctrs_type_logical: {
-    int* x_ptr = LOGICAL(x);
-    int* y_ptr = LOGICAL(y);
-    for (R_len_t i = 0; i < n; ++i, ++x_ptr, ++y_ptr) {
-      p_out[i] = lgl_equal_scalar(x_ptr, y_ptr, na_equal);
-    }
-  }
-  case vctrs_type_integer: {
-    int* x_ptr = INTEGER(x);
-    int* y_ptr = INTEGER(y);
-    for (R_len_t i = 0; i < n; ++i, ++x_ptr, ++y_ptr) {
-      p_out[i] = int_equal_scalar(x_ptr, y_ptr, na_equal);
-    }
-    break;
-  }
-  case vctrs_type_double: {
-    double* x_ptr = REAL(x);
-    double* y_ptr = REAL(y);
-    for (R_len_t i = 0; i < n; ++i, ++x_ptr, ++y_ptr) {
-      p_out[i] = dbl_equal_scalar(x_ptr, y_ptr, na_equal);
-    }
-    break;
-  }
-  case vctrs_type_character: {
-    SEXP* x_ptr = STRING_PTR(x);
-    SEXP* y_ptr = STRING_PTR(y);
-    for (R_len_t i = 0; i < n; ++i, ++x_ptr, ++y_ptr) {
-      p_out[i] = chr_equal_scalar(x_ptr, y_ptr, na_equal);
-    }
-    break;
-  }
-  case vctrs_type_list: {
-    for (R_len_t i = 0; i < n; ++i) {
-      p_out[i] = list_equal_scalar(x, i, y, i, na_equal);
-    }
-    break;
-  }
-  case vctrs_type_dataframe: {
-    for (R_len_t i = 0; i < n; ++i) {
-      p_out[i] = df_equal_scalar(x, i, y, i, na_equal);
-    }
-    break;
-  }
-  case vctrs_type_scalar:
-    Rf_errorcall(R_NilValue, "Can't compare scalars with `vctrs_equal()`");
-  default:
-    Rf_error("Unimplemented type in `vctrs_equal()`");
-  }
-
-  UNPROTECT(1);
-  return out;
-}
-
 // [[ register() ]]
 SEXP vctrs_equal_na(SEXP x) {
   R_len_t n = vec_size(x);
@@ -361,9 +379,4 @@ SEXP vctrs_equal_na(SEXP x) {
 
   UNPROTECT(1);
   return out;
-}
-
-// [[ register() ]]
-SEXP vctrs_equal_object(SEXP x, SEXP y, SEXP na_equal) {
-  return Rf_ScalarLogical(equal_object(x, y, Rf_asLogical(na_equal)));
 }
