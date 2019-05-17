0e21e3c73b96bcb884eab581a838b72867d4c45e
Lionel Henry <lionel.hry@gmail.com>
2019-05-17 11:13:05 +0200

    Note how `hash_object()` could be optimised

--- src/hash.c
+++ src/hash.c
@@ -94,6 +94,7 @@ int32_t hash_object(SEXP x) {
   case VECSXP: {
     R_len_t n = vec_size(x);
     for (R_len_t i = 0; i < n; ++i) {
+      // FIXME: Call SEXP dereferencers outside of the loop
       hash = hash_combine(hash, hash_scalar(x, i));
     }
     break;
