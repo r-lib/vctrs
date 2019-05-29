29b333222d999980b03a0c042ec81ea427f48753
Kirill Müller <krlmlr+r@mailbox.org>
2019-05-29 23:33:32 +0200

    Life cycle for vec_dim()

--- R/dim.R
+++ R/dim.R
@@ -1,12 +1,20 @@
 #' Vector dimensions
 #'
+#' @description
+#' \Sexpr[results=rd, stage=render]{vctrs:::lifecycle("maturing")}
+#'
 #' * `vec_empty()` returns `TRUE` if `vec_size()` is zero.
 #' * `vec_dims()` gives the dimensionality (i.e. number of dimensions)
 #' * `vec_dim()` returns the size of each dimension
 #'
+#' @details
 #' Unlike base R, we treat vectors with `NULL` dimensions as 1d. This
 #' simplifies the type system by eliding a special case. Compared to base R
 #' equivalent, `vec_dim()` returns `length()`, not `NULL`, when `x` is 1d.
+#'
+#' @section Life cycle:
+#'
+#' `vec_dim()` is maturing, because it is used in the pillar package.
 #'
 #' @param x A vector
 #' @name dims
