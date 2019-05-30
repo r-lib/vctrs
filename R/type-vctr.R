e559af4680d157d3a19f4bf29883296416d363ee
Davis Vaughan <davis@rstudio.com>
2019-05-30 05:45:34 -0400

    Fix `min()` and `max()` methods (#329)
    Use `vec_equal()` to find max and min value positions

--- R/type-vctr.R
+++ R/type-vctr.R
@@ -403,7 +403,13 @@ quantile.vctrs_vctr <- function(x, ..., type = 1, na.rm = FALSE) {
 min.vctrs_vctr <- function(x, ..., na.rm = FALSE) {
   # TODO: implement to do vec_arg_min()
   rank <- xtfrm(x)
-  idx <- if (isTRUE(na.rm)) which.max(rank) else which(rank == min(rank))
+
+  if (isTRUE(na.rm)) {
+    idx <- which.min(rank)
+  } else {
+    idx <- which(vec_equal(rank, min(rank), na_equal = TRUE))
+  }
+
   x[[idx[[1]]]]
 }
 
@@ -411,7 +417,13 @@ min.vctrs_vctr <- function(x, ..., na.rm = FALSE) {
 max.vctrs_vctr <- function(x, ..., na.rm = FALSE) {
   # TODO: implement to do vec_arg_max()
   rank <- xtfrm(x)
-  idx <- if (isTRUE(na.rm)) which.max(rank) else which(rank == max(rank))
+
+  if (isTRUE(na.rm)) {
+    idx <- which.max(rank)
+  } else {
+    idx <- which(vec_equal(rank, max(rank), na_equal = TRUE))
+  }
+
   x[[idx[[1]]]]
 }
 
