40271403b3638e89981b140a7ff7bce3b62269d2
Kirill Müller <krlmlr+r@mailbox.org>
2019-06-06 17:17:55 +0200

    Use isdigit()

--- src/names.c
+++ src/names.c
@@ -213,22 +213,9 @@ static bool is_dotdotint(const char* name) {
   return (bool) strtol(name, NULL, 10);
 }
 
-static bool is_digit(const char c) {
-  switch (c) {
-  case '0':
-  case '1':
-  case '2':
-  case '3':
-  case '4':
-  case '5':
-  case '6':
-  case '7':
-  case '8':
-  case '9':
-    return true;
-  default:
-    return false;
-  }
+// Wrap for casting to unsigned char
+static bool is_digit(const unsigned char c) {
+  return isdigit(c);
 }
 
 static ptrdiff_t suffix_pos(const char* name) {
