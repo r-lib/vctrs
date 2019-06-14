ffeb3d0d4f7bded46b3196d26eaf700c8179b12f
DavisVaughan <davis@rstudio.com>
2019-06-14 16:52:33 -0400

    Add more recycling corner cases

--- tests/testthat/test-shape.R
+++ tests/testthat/test-shape.R
@@ -30,6 +30,7 @@ test_that("recycling rules applied", {
   expect_equal(shape_common(int(1, 5, 1), int(1, 1, 5)), c(5L, 5L))
   expect_equal(shape_common(int(1, 1, 1), int(1, 5, 5)), c(5L, 5L))
 
+  expect_equal(shape_common(int(1, 0, 5), int(1, 1, 1)), c(0L, 5L))
   expect_error(shape_common(int(1, 0, 5), int(1, 5, 1)), "0, 5")
   expect_error(shape_common(int(1, 5, 0), int(1, 1, 5)), "0, 5")
 })
@@ -65,8 +66,19 @@ test_that("recycling rules applied", {
     matrix(1:4, 4, 4)
   )
 
+  expect_equal(
+    shape_broadcast(array(1L, c(1, 1)), int(1, 0)),
+    matrix(integer(), nrow = 1)
+  )
+
   expect_error(
     shape_broadcast(array(1L, c(1, 2)), int(1, 0)),
+    "Non-recyclable dimensions",
+    class = "vctrs_error_incompatible_cast"
+  )
+
+  expect_error(
+    shape_broadcast(array(1L, c(1, 0)), int(1, 1)),
     "Non-recyclable dimensions",
     class = "vctrs_error_incompatible_cast"
   )
