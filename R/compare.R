8f58cf3f540fac628b81a4a51525b708c6c1b44e
DavisVaughan <davis@rstudio.com>
2019-06-14 08:56:10 -0400

    Construct the `base::order()` call with `expr()` and `!!!`

--- R/compare.R
+++ R/compare.R
@@ -143,10 +143,10 @@ order_proxy <- function(proxy, direction = "asc", na_value = "largest") {
 
   if (is.data.frame(proxy)) {
     args <- unname(proxy)
-    args$decreasing <- decreasing
-    args$na.last <- na.last
-
-    do.call(base::order, args)
+    order_expr <- expr(
+      base::order(!!! args, decreasing = decreasing, na.last = na.last)
+    )
+    eval_bare(order_expr)
   } else if (is_character(proxy) || is_logical(proxy) || is_integer(proxy) || is_double(proxy)) {
     order(proxy, decreasing = decreasing, na.last = na.last)
   } else {
