e6f7c803137137007b3589e19ef7b37407733a1d
Kirill Müller <krlmlr+r@mailbox.org>
2019-06-29 16:37:50 +0200

    Export dim2()

--- R/dim.R
+++ R/dim.R
@@ -12,6 +12,10 @@
 #' simplifies the type system by eliding a special case. Compared to the base R
 #' equivalent, `vec_dim()` returns [length()], not `NULL`, when `x` is 1d.
 #'
+#' @seealso
+#' [dim2()], a variant of [dim()] that returns [length()] if an object
+#' doesn't have dimensions.
+#'
 #' @param x A vector
 #' @name dim
 #' @examples
@@ -33,8 +37,27 @@ vec_dim_n <- function(x) {
   .Call(vctrs_dim_n, x)
 }
 
-# Unexported counterpart to `vec_dim()` that respects that `dim()` is a generic.
-# Data frames looks like 2d structures with this.
+#' Perceived vector dimensions
+#'
+#' @description
+#' `dim2()` is a variant of [dim()] that returns [vec_size()] if an object
+#' doesn't have dimensions.
+#'
+#' @details
+#' Unlike base R, we treat vectors with `NULL` dimensions as 1d. This
+#' simplifies the type system by eliding a special case. Compared to the base R
+#' equivalent, `dim2()` returns [length()], not `NULL`, when `x` is 1d.
+#'
+#' @seealso
+#' [vec_dim()], a variant that never dispatches over the [dim()] generic.
+#'
+#' @param x A vector
+#' @export
+#' @examples
+#' # Compared to base R
+#' x <- 1:5
+#' dim(x)
+#' vec_dim(x)
 dim2 <- function(x) {
   dim(x) %||% length(x)
 }
