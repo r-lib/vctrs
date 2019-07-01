2c9a3786ec85c43fb0799bf63c5a70d2022b1689
Lionel Henry <lionel.hry@gmail.com>
2019-07-01 17:10:38 +0200

    Register tibble methods conditionally
    Closes #437

--- R/type-tibble.R
+++ R/type-tibble.R
@@ -1,25 +1,16 @@
 # Coercion ----------------------------------------------------------------
 
-#' @rdname vec_ptype2
-#' @export vec_ptype2.tbl_df
-#' @method vec_ptype2 tbl_df
-#' @export
-vec_ptype2.tbl_df <- function(x, y, ...) UseMethod("vec_ptype2.tbl_df", y)
-
-#' @method vec_ptype2.tbl_df data.frame
-#' @export
-vec_ptype2.tbl_df.data.frame <- function(x, y, ..., x_arg = "x", y_arg = "y") {
-  .Call(vctrs_type2_df_df, x, y, x_arg, y_arg)
+# Conditionally registered in .onLoad()
+vec_ptype2.tbl_df <- function(x, y, ...) {
+  UseMethod("vec_ptype2.tbl_df", y)
 }
-#' @method vec_ptype2.data.frame tbl_df
-#' @export
-vec_ptype2.data.frame.tbl_df <- function(x, y, ..., x_arg = "x", y_arg = "y") {
-  .Call(vctrs_type2_df_df, x, y, x_arg, y_arg)
-}
-
-#' @method vec_ptype2.tbl_df default
-#' @export
 vec_ptype2.tbl_df.default <- function(x, y, ...) {
   # FIXME: Do we need some sort of `next_vec_type2()`?
   vec_ptype2.data.frame(x, y)
+}
+vec_ptype2.tbl_df.data.frame <- function(x, y, ..., x_arg = "x", y_arg = "y") {
+  .Call(vctrs_type2_df_df, x, y, x_arg, y_arg)
+}
+vec_ptype2.data.frame.tbl_df <- function(x, y, ..., x_arg = "x", y_arg = "y") {
+  .Call(vctrs_type2_df_df, x, y, x_arg, y_arg)
 }
--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,14 @@
   s3_register("generics::as.ordered", "vctrs_vctr")
   s3_register("generics::as.difftime", "vctrs_vctr")
 
+  # Remove once tibble has implemented the methods
+  if (is_installed("tibble") && !env_has(ns_env("tibble"), "vec_ptype2.tbl_df")) {
+    s3_register("vctrs::vec_ptype2", "tbl_df")
+    s3_register("vctrs::vec_ptype2.tbl_df", "default")
+    s3_register("vctrs::vec_ptype2.tbl_df", "data.frame")
+    s3_register("vctrs::vec_ptype2.data.frame", "tbl_df")
+  }
+
   utils::globalVariables("vec_set_attributes")
 
   # Prevent two copies from being made by `attributes(x) <- attrib` on R < 3.6.0
