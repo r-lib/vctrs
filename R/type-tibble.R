20a582e00940cbccfeff49bede27d2a69375e775
Lionel Henry <lionel.hry@gmail.com>
2019-11-25 14:17:55 +0100

    Forward ptype2 args to super method

--- R/type-tibble.R
+++ R/type-tibble.R
@@ -11,7 +11,7 @@ vec_ptype2.tbl_df <- function(x, y, ...) {
 }
 vec_ptype2.tbl_df.default <- function(x, y, ...) {
   # FIXME: Do we need some sort of `next_vec_type2()`?
-  vec_ptype2.data.frame(x, y)
+  vec_ptype2.data.frame(x, y, ...)
 }
 
 vec_ptype2.tbl_df.data.frame <- function(x, y, ..., x_arg = "x", y_arg = "y") {
