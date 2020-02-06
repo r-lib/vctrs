f122ff6e8fae336cf13e39c486332bf007d63c79
DavisVaughan <davis@rstudio.com>
2020-02-06 16:42:48 -0500

    Declare C API functions with `extern`

--- inst/include/vctrs.h
+++ inst/include/vctrs.h
@@ -5,13 +5,13 @@
 #include <R_ext/Rdynload.h>
 #include <stdbool.h>
 
-SEXP (*vec_proxy)(SEXP);
-SEXP (*vec_restore)(SEXP, SEXP, SEXP);
-SEXP (*vec_assign_impl)(SEXP, SEXP, SEXP, bool);
-SEXP (*vec_slice_impl)(SEXP, SEXP);
-SEXP (*vec_names)(SEXP);
-SEXP (*vec_set_names)(SEXP, SEXP);
-SEXP (*vec_chop)(SEXP, SEXP);
+extern SEXP (*vec_proxy)(SEXP);
+extern SEXP (*vec_restore)(SEXP, SEXP, SEXP);
+extern SEXP (*vec_assign_impl)(SEXP, SEXP, SEXP, bool);
+extern SEXP (*vec_slice_impl)(SEXP, SEXP);
+extern SEXP (*vec_names)(SEXP);
+extern SEXP (*vec_set_names)(SEXP, SEXP);
+extern SEXP (*vec_chop)(SEXP, SEXP);
 
 void vctrs_init_api();
 
