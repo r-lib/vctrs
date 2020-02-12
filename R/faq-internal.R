0af6043061deacdb690d23bd6f088e1727ae07c7
Lionel Henry <lionel.hry@gmail.com>
2020-02-12 18:35:13 +0100

    Add internal FAQ about identity element of `vec_ptype2()`

---
+++ R/faq-internal.R
@@ -0,0 +1,6 @@
+#' Internal FAQ - `vec_ptype2()`, `NULL`, and unspecified vectors
+#'
+#' @includeRmd man/faq/internal/ptype2-identity.Rmd description
+#'
+#' @name internal-faq-ptype2-identity
+NULL
