6f2d0aefe070353370b7772a4c5e5b385c418bf5
Davis Vaughan <davis@rstudio.com>
2020-02-18 10:40:27 -0500

    Optimize `new_data_frame()` and add `new_bare_data_frame()` (#830)
    * Use `list()`, not `pairlist()` for attributes
    
    * Inline formals of `new_data_frame()`
    
    * Add `new_bare_data_frame()`
    
    * Ensure alignment between code and docs
    
    * Add tests for `new_bare_data_frame()`

--- R/type-data-frame.R
+++ R/type-data-frame.R
@@ -15,9 +15,18 @@
 #' @keywords internal
 #' @examples
 #' new_data_frame(list(x = 1:10, y = 10:1))
-new_data_frame <- function(x = list(), n = NULL, ..., class = character()) {
-  .Call(vctrs_new_data_frame, x, n, pairlist(...), class)
+new_data_frame <- function(x = list(), n = NULL, ..., class = character(0)) {
+  .Call(vctrs_new_data_frame, x, n, list(...), class)
 }
+new_data_frame <- fn_inline_formals(new_data_frame, c("x", "n", "class"))
+
+#' @rdname new_data_frame
+#' @export
+new_bare_data_frame <- function(x = list(), n = NULL) {
+  .Call(vctrs_new_bare_data_frame, x, n)
+}
+new_bare_data_frame <- fn_inline_formals(new_bare_data_frame, c("x", "n"))
+
 
 # Light weight constructor used for tests - avoids having to repeatedly do
 # stringsAsFactors = FALSE etc. Should not be used in internal code as is
