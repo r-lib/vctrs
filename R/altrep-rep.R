3d21356ad020ab62a95add2e552fe15cfad30b32
DavisVaughan <davis@rstudio.com>
2020-02-19 14:50:04 -0500

    Implement `vctrs_compact_rep_dbl`

--- R/altrep-rep.R
+++ R/altrep-rep.R
@@ -3,3 +3,9 @@ new_altrep_vctrs_compact_rep_int <- function(value, size) {
   size <- vec_cast(vec_cast(size, integer()), double())
   .Call(vctrs_new_altrep_vctrs_compact_rep_int, value, size)
 }
+
+new_altrep_vctrs_compact_rep_dbl <- function(value, size) {
+  value <- vec_cast(value, double())
+  size <- vec_cast(vec_cast(size, integer()), double())
+  .Call(vctrs_new_altrep_vctrs_compact_rep_dbl, value, size)
+}
