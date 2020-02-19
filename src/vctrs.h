0e8be0c78a5eb43bccef861a35d7f5e23355f81c
DavisVaughan <davis@rstudio.com>
2020-02-18 23:13:21 -0500

    Add compat for elt functions that should never get called on R < 3.5.0

--- src/vctrs.h
+++ src/vctrs.h
@@ -649,6 +649,12 @@ void stop_corrupt_ordered_levels(SEXP x, struct vctrs_arg* arg) __attribute__((n
 # define COMPLEX_RO(x) ((const Rcomplex*) COMPLEX(x))
 # define STRING_PTR_RO(x) ((const SEXP*) STRING_PTR(x))
 # define RAW_RO(x) ((const Rbyte*) RAW(x))
+
+# define LOGICAL_ELT(x, i) (LOGICAL(x)[i])
+# define INTEGER_ELT(x, i) (INTEGER(x)[i])
+# define REAL_ELT(x, i)    (REAL(x)[i])
+# define COMPLEX_ELT(x, i) (COMPLEX(x)[i])
+# define RAW_ELT(x, i)     (RAW(x)[i])
 #endif
 
 
