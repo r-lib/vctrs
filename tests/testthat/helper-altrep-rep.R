69f5b3fad814b4c8478ba4317b59d1ec55ce0eeb
DavisVaughan <davis@rstudio.com>
2020-02-21 21:17:11 -0500

    Constructors should be less lenient

--- tests/testthat/helper-altrep-rep.R
+++ tests/testthat/helper-altrep-rep.R
@@ -1,23 +1,23 @@
 new_altrep_vctrs_compact_rep_lgl <- function(value, size) {
-  value <- vec_cast(value, logical())
+  value <- vec_assert(value, logical())
   size <- vec_cast(vec_cast(size, integer()), double())
   .Call(vctrs_new_altrep_vctrs_compact_rep_lgl, value, size)
 }
 
 new_altrep_vctrs_compact_rep_int <- function(value, size) {
-  value <- vec_cast(value, integer())
+  value <- vec_assert(value, integer())
   size <- vec_cast(vec_cast(size, integer()), double())
   .Call(vctrs_new_altrep_vctrs_compact_rep_int, value, size)
 }
 
 new_altrep_vctrs_compact_rep_dbl <- function(value, size) {
-  value <- vec_cast(value, double())
+  value <- vec_assert(value, double())
   size <- vec_cast(vec_cast(size, integer()), double())
   .Call(vctrs_new_altrep_vctrs_compact_rep_dbl, value, size)
 }
 
 new_altrep_vctrs_compact_rep_chr <- function(value, size) {
-  value <- vec_cast(value, character())
+  value <- vec_assert(value, character())
   size <- vec_cast(vec_cast(size, integer()), double())
   .Call(vctrs_new_altrep_vctrs_compact_rep_chr, value, size)
 }
