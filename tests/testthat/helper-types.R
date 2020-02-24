d0dd321f14c3ca7e7dded9c90efcde34d7b8ce10
DavisVaughan <davis@rstudio.com>
2020-02-24 15:36:33 -0500

    Ensure that tests are compliant with upcoming `stringsAsFactors` changes

--- tests/testthat/helper-types.R
+++ tests/testthat/helper-types.R
@@ -91,3 +91,13 @@ c_na <- function(...) {
   names(x)[names(x) == ""] <- NA_character_
   x
 }
+
+# We almost never want `stringsAsFactors = TRUE`, and `FALSE` became
+# the default in R 4.0.0. This wrapper ensures that our tests are compliant
+# with versions of R before and after this change.
+if (getRversion() < "4.0.0") {
+  data.frame <- function(..., stringsAsFactors = NULL) {
+    stringsAsFactors <- stringsAsFactors %||% FALSE
+    base::data.frame(..., stringsAsFactors = stringsAsFactors)
+  }
+}
