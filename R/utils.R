1e4bacb41208ec217b6e1709eec29e88662ce4a2
Davis Vaughan <davis@rstudio.com>
2020-02-25 10:27:49 -0500

    Ensure that tests are compliant with upcoming `stringsAsFactors` changes (#856)
    * Ensure that tests are compliant with upcoming `stringsAsFactors` changes
    
    * Move `data.frame()` wrapper to `utils.R` to avoid testthat source ordering
    
    * Unconditionally use our `data.frame()` wrapper

--- R/utils.R
+++ R/utils.R
@@ -131,3 +131,13 @@ unstructure <- function(x) {
   attributes(x) <- NULL
   x
 }
+
+# We almost never want `stringsAsFactors = TRUE`, and `FALSE` became
+# the default in R 4.0.0. This wrapper ensures that our tests are compliant
+# with versions of R before and after this change. Keeping it in `utils.R`
+# rather than as a testthat helper ensures that it is sourced before any other
+# testthat helpers.
+data.frame <- function(..., stringsAsFactors = NULL) {
+  stringsAsFactors <- stringsAsFactors %||% FALSE
+  base::data.frame(..., stringsAsFactors = stringsAsFactors)
+}
