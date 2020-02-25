353ffaa5c3b03b490a53ae372522f6c6ede4be7d
DavisVaughan <davis@rstudio.com>
2020-02-25 10:13:37 -0500

    Unconditionally use our `data.frame()` wrapper

--- R/utils.R
+++ R/utils.R
@@ -134,10 +134,10 @@ unstructure <- function(x) {
 
 # We almost never want `stringsAsFactors = TRUE`, and `FALSE` became
 # the default in R 4.0.0. This wrapper ensures that our tests are compliant
-# with versions of R before and after this change.
-if (getRversion() < "4.0.0") {
-  data.frame <- function(..., stringsAsFactors = NULL) {
-    stringsAsFactors <- stringsAsFactors %||% FALSE
-    base::data.frame(..., stringsAsFactors = stringsAsFactors)
-  }
+# with versions of R before and after this change. Keeping it in `utils.R`
+# rather than as a testthat helper ensures that it is sourced before any other
+# testthat helpers.
+data.frame <- function(..., stringsAsFactors = NULL) {
+  stringsAsFactors <- stringsAsFactors %||% FALSE
+  base::data.frame(..., stringsAsFactors = stringsAsFactors)
 }
