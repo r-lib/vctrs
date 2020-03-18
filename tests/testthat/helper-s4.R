3f74fa6c6ba88e98bf9dfa579dc8697a4105f4da
Lionel Henry <lionel.hry@gmail.com>
2020-03-18 16:30:24 +0100

    Add `local_s4_method()` helper

--- tests/testthat/helper-s4.R
+++ tests/testthat/helper-s4.R
@@ -22,3 +22,29 @@ setMethod("[", "vctrs_rando", function(x, i, j, ..., drop = TRUE) {
   contains = "integer",
   slots = c(name = "character")
 )
+
+local_s4_method <- function(generic, signature, method, frame = caller_env()) {
+  methods::setMethod(generic, signature, method)
+  exit_expr <- call2(methods::removeMethod, generic, signature, where = topenv(frame))
+  local_exit(exit_expr, frame = frame)
+}
+with_s4_method <- function(generic, signature, method, expr) {
+  local_s4_method(generic, signature, method)
+  expr
+}
+
+local_exit <- function(expr, frame = caller_env()) {
+  # We are at top-level when only one frame refers to the global environment
+  if (is_reference(frame, global_env())) {
+    is_global_frame <- sys.parents() == 0
+    if (sum(is_global_frame) == 1) {
+      abort("Can't add an exit event at top-level")
+    }
+  }
+
+  # Inline everything so the call will succeed in any environment
+  expr <- call2(on.exit, expr, add = TRUE)
+  eval_bare(expr, frame)
+
+  invisible(expr)
+}
