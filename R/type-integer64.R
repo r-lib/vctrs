ca72885cab3d9eb00d28254e578c89816b09fd7f
Lionel Henry <lionel.hry@gmail.com>
2020-03-26 12:59:34 +0100

    Remove unspecified methods
    Closes #952

--- R/type-integer64.R
+++ R/type-integer64.R
@@ -38,10 +38,6 @@ vec_ptype2.integer64 <- function(x, y, ...) {
   UseMethod("vec_ptype2.integer64")
 }
 
-#' @method vec_ptype2.integer64 vctrs_unspecified
-#' @export
-vec_ptype2.integer64.vctrs_unspecified <- function(x, y, ...) bit64::integer64()
-
 #' @method vec_ptype2.integer64 integer64
 #' @export
 vec_ptype2.integer64.integer64 <- function(x, y, ...) bit64::integer64()
