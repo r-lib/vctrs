728dd802e6a9fee04b83a7e8dd2ebd7f0bf4dbba
Lionel Henry <lionel.hry@gmail.com>
2020-03-27 19:41:03 +0100

    Fix experimental badge

--- R/bind.R
+++ R/bind.R
@@ -172,7 +172,7 @@ as_df_col <- function(x, outer_name) {
 #'
 #' @description
 #'
-#' \Sexpr[results=rd, stage=render]{mypkg:::lifecycle("experimental")}
+#' \Sexpr[results=rd, stage=render]{vctrs:::lifecycle("experimental")}
 #'
 #' This is an experimental generic that returns zero-columns variants
 #' of a data frame. It is needed for [vec_cbind()], to work around the
