2f56e195d7a16410467d790e7a895aae0f7401f7
DavisVaughan <davis@rstudio.com>
2020-03-29 15:07:55 -0400

    Remove list to data frame cast

--- tests/testthat/test-type-data-frame.R
+++ tests/testthat/test-type-data-frame.R
@@ -133,14 +133,9 @@ test_that("can cast unspecified to data frame", {
   expect_identical(vec_cast(unspecified(3), df), vec_init(df, 3))
 })
 
-test_that("can cast list of data frames to data frame", {
+test_that("cannot cast list to data frame", {
   df <- data.frame(x = 1, y = 2L)
-  expect_equal(vec_cast(list(df, df), df), vec_slice(df, c(1, 1)))
-})
-
-test_that("can only cast list of data frames to data frame if they are all size 1", {
-  df <- data.frame(x = 1:2)
-  expect_error(vec_cast(list(df), df), class = "vctrs_error_cast_lossy")
+  expect_error(vec_cast(list(df, df), df), class = "vctrs_error_incompatible_cast")
 })
 
 test_that("can restore lists with empty names", {
