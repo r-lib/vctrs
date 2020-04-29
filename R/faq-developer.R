c085130642808181a658793248ae818c628691be
Lionel Henry <lionel.hry@gmail.com>
2020-04-29 18:01:38 +0200

    Document process for implementing coercion methods

---
+++ R/faq-developer.R
@@ -0,0 +1,13 @@
+#' FAQ - How does coercion work in vctrs?
+#'
+#' @includeRmd man/faq/developer/theory-coercion.Rmd description
+#'
+#' @name theory-faq-coercion
+NULL
+
+#' FAQ - How to implement ptype2 and cast methods?
+#'
+#' @includeRmd man/faq/developer/howto-coercion.Rmd description
+#'
+#' @name howto-faq-coercion
+NULL
