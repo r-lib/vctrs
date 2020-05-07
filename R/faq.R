878264f6e1fce65160c29ec2f918011660189deb
Lionel Henry <lionel.hry@gmail.com>
2020-05-07 16:44:33 +0200

    Add FAQ page about incompatible attributes
    Part of #1016

--- R/faq.R
+++ R/faq.R
@@ -2,10 +2,14 @@
 #'
 #' @description
 #'
-#' TODO. Tracked in r-lib/vctrs#1016
-#'
 #' This error occurs when [vec_ptype2()] or [vec_cast()] are supplied
-#' vectors of the same classes with different attributes.
+#' vectors of the same classes with different attributes. In this
+#' case, vctrs doesn't know how to combine the inputs.
+#'
+#' To fix this error, the maintainer of the class should implement
+#' self-to-self coercion methods for [vec_ptype2()] and [vec_cast()].
+#'
+#' @includeRmd man/faq/developer/links-coercion.Rmd
 #'
 #' @name faq-error-incompatible-attributes
 NULL
