799dcb430259fd6575cfcba31159ba4cbebaff3a
DavisVaughan <davis@rstudio.com>
2020-06-03 13:24:48 -0400

    Remove `vctrs_int_radix_sort`

--- src/init.c
+++ src/init.c
@@ -122,7 +122,6 @@ extern SEXP vctrs_implements_ptype2(SEXP);
 extern SEXP vctrs_ptype2_dispatch_native(SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP vctrs_cast_dispatch_native(SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP vctrs_fast_c(SEXP, SEXP);
-extern SEXP vctrs_int_radix_sort(SEXP);
 extern SEXP vctrs_radix_order(SEXP);
 
 
@@ -263,7 +262,6 @@ static const R_CallMethodDef CallEntries[] = {
   {"vctrs_ptype2_dispatch_native",     (DL_FUNC) &vctrs_ptype2_dispatch_native, 5},
   {"vctrs_cast_dispatch_native",       (DL_FUNC) &vctrs_cast_dispatch_native, 5},
   {"vctrs_fast_c",                     (DL_FUNC) &vctrs_fast_c, 2},
-  {"vctrs_int_radix_sort",             (DL_FUNC) &vctrs_int_radix_sort, 1},
   {"vctrs_radix_order",                (DL_FUNC) &vctrs_radix_order, 1},
   {NULL, NULL, 0}
 };
