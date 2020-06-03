18c865272cfbd5ed4eea1463fc08327e38060db7
DavisVaughan <davis@rstudio.com>
2020-06-03 16:14:00 -0400

    Add `decreasing` option, vectorized for data frames

--- src/init.c
+++ src/init.c
@@ -122,7 +122,7 @@ extern SEXP vctrs_implements_ptype2(SEXP);
 extern SEXP vctrs_ptype2_dispatch_native(SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP vctrs_cast_dispatch_native(SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP vctrs_fast_c(SEXP, SEXP);
-extern SEXP vctrs_radix_order(SEXP, SEXP);
+extern SEXP vctrs_radix_order(SEXP, SEXP, SEXP);
 
 
 // Maturing
@@ -262,7 +262,7 @@ static const R_CallMethodDef CallEntries[] = {
   {"vctrs_ptype2_dispatch_native",     (DL_FUNC) &vctrs_ptype2_dispatch_native, 5},
   {"vctrs_cast_dispatch_native",       (DL_FUNC) &vctrs_cast_dispatch_native, 5},
   {"vctrs_fast_c",                     (DL_FUNC) &vctrs_fast_c, 2},
-  {"vctrs_radix_order",                (DL_FUNC) &vctrs_radix_order, 2},
+  {"vctrs_radix_order",                (DL_FUNC) &vctrs_radix_order, 3},
   {NULL, NULL, 0}
 };
 
