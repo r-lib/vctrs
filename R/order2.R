29f6eb63a94a89a8e4b4c285564720e3a31c5698
DavisVaughan <davis@rstudio.com>
2020-06-08 16:50:10 -0400

    Export old method for comparitive testing

--- R/order2.R
+++ R/order2.R
@@ -1,3 +1,8 @@
 vec_radix_order <- function(x, decreasing = FALSE, na_last = TRUE) {
   .Call(vctrs_radix_order, x, decreasing, na_last)
 }
+
+vec_radix_order_old <- function(x, na_last = TRUE, decreasing = FALSE) {
+  .Call(vctrs_radix_order_old, x, na_last, decreasing)
+}
+
