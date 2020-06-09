162020d5adfc9206331eb67f616ca0f548cd97bf
DavisVaughan <davis@rstudio.com>
2020-06-09 15:51:51 -0400

    Remove old order files

--- R/order.R
+++ R/order.R
@@ -1,3 +1,7 @@
-# vec_radix_order <- function(x, na_last = TRUE, decreasing = FALSE) {
-#   .Call(vctrs_radix_order, x, na_last, decreasing)
-# }
+vec_radix_order <- function(x, decreasing = FALSE, na_last = TRUE) {
+  .Call(vctrs_radix_order, x, decreasing, na_last, FALSE)
+}
+
+vec_radix_order_groups <- function(x, decreasing = FALSE, na_last = TRUE) {
+  .Call(vctrs_radix_order, x, decreasing, na_last, TRUE)
+}
