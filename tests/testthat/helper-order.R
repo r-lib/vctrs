24b4537b7d9903b12c8da8179de097db56b3f14b
DavisVaughan <davis@rstudio.com>
2020-06-12 11:36:12 -0400

    Add first draft of character radix ordering

--- tests/testthat/helper-order.R
+++ tests/testthat/helper-order.R
@@ -3,6 +3,21 @@ GROUP_DATA_SIZE_DEFAULT <- 100000L
 INSERTION_ORDER_BOUNDARY <- 128L
 INT_COUNTING_ORDER_RANGE_BOUNDARY <- 100000L
 
+# Force radix method for character comparisons
 lst_order <- function(x, na.last = TRUE, decreasing = FALSE) {
-  rlang::exec("order", !!!unname(x), na.last = na.last, decreasing = decreasing)
+  rlang::exec(
+    "order",
+    !!!unname(x),
+    na.last = na.last,
+    decreasing = decreasing,
+    method = "radix"
+  )
+}
+
+base_order <- function(x, na.last = TRUE, decreasing = FALSE) {
+  if (is.data.frame(x)) {
+    lst_order(x, na.last = na.last, decreasing = decreasing)
+  } else {
+    order(x, na.last = na.last, decreasing = decreasing, method = "radix")
+  }
 }
