c57bd4b7daebaf7376926504276b84d0abb162d1
DavisVaughan <davis@rstudio.com>
2020-06-16 15:55:33 -0400

    Move lazy vec helpers to `order-lazy.h`

--- src/order-lazy.h
+++ src/order-lazy.h
@@ -21,5 +21,32 @@ struct lazy_vec {
   *(p_n) += 1;                                                 \
 } while (0)
 
+
+// Pair with `PROTECT_LAZY_VEC()`
+static inline struct lazy_vec new_lazy_vec(R_xlen_t size, size_t multiplier) {
+  struct lazy_vec out;
+
+  out.data = vctrs_shared_empty_raw;
+
+  out.size = size * multiplier;
+  out.initialized = false;
+
+  return out;
+}
+
+static inline void lazy_vec_initialize(struct lazy_vec* p_x) {
+  if (p_x->initialized) {
+    return;
+  }
+
+  p_x->data = Rf_allocVector(RAWSXP, p_x->size);
+
+  REPROTECT(p_x->data, p_x->data_pi);
+
+  p_x->p_data = DATAPTR(p_x->data);
+
+  p_x->initialized = true;
+}
+
 // -----------------------------------------------------------------------------
 #endif
