5eb14ebd91ee2505b09ded3121a2163072a10a4b
DavisVaughan <davis@rstudio.com>
2020-06-24 11:13:26 -0400

    Unconditionally clone ALTREP objects

--- src/owned.h
+++ src/owned.h
@@ -17,12 +17,16 @@ static inline enum vctrs_owned vec_owned(SEXP x) {
 }
 
 // Wrapper around `r_clone_referenced()` that only attempts to clone if
-// we indicate that we don't own `x`, or if we do own `x` but it is ALTREP.
-// If `x` is ALTREP, we must clone it before dereferencing, otherwise we get
-// a pointer into the ALTREP internals rather than into the object it
-// truly represents.
+// we indicate that we don't own `x`, or if `x` is ALTREP.
+// If `x` is ALTREP, we must unconditionally clone it before dereferencing,
+// otherwise we get a pointer into the ALTREP internals rather than into the
+// object it truly represents.
 static inline SEXP vec_clone_referenced(SEXP x, const enum vctrs_owned owned) {
-  if (owned == VCTRS_OWNED_false || ALTREP(x)) {
+  if (ALTREP(x)) {
+    return r_clone_referenced(x);
+  }
+
+  if (owned == VCTRS_OWNED_false) {
     return r_clone_referenced(x);
   } else {
     return x;
