d2a4fc8125bf1022d8d2e55035841ca74cb41c2d
Lionel Henry <lionel.hry@gmail.com>
2020-07-09 14:38:19 +0200

    Add vctrs methods for `omit` and `exclude` (#1179)
    Closes #1160

--- R/type-misc.R
+++ R/type-misc.R
@@ -5,6 +5,114 @@
 vec_proxy.numeric_version <- function(x, ...) x
 
 
+# `omit` from base ---------------------------------------------------
+
+#' @export
+vec_proxy.omit <- function(x, ...) {
+  x
+}
+#' @export
+vec_restore.omit <- function(x, ...) {
+  structure(x, class = "omit")
+}
+
+#' @export
+vec_ptype2.omit.omit <- function(x, y, ...) {
+  x
+}
+#' @export
+vec_ptype2.integer.omit <- function(x, y, ...) {
+  x
+}
+#' @export
+vec_ptype2.omit.integer <- function(x, y, ...) {
+  y
+}
+#' @export
+vec_ptype2.double.omit <- function(x, y, ...) {
+  x
+}
+#' @export
+vec_ptype2.omit.double <- function(x, y, ...) {
+  y
+}
+
+#' @export
+vec_cast.omit.omit <- function(x, to, ...) {
+  x
+}
+#' @export
+vec_cast.integer.omit <- function(x, to, ...) {
+  vec_cast(vec_data(x), to, ...)
+}
+#' @export
+vec_cast.omit.integer <- function(x, to, ..., x_arg = "", to_arg = "") {
+  stop_incompatible_cast(x, to, x_arg = x_arg, to_arg = to_arg)
+}
+#' @export
+vec_cast.double.omit <- function(x, to, ...) {
+  vec_cast(vec_data(x), to, ...)
+}
+#' @export
+vec_cast.omit.double <- function(x, to, ..., x_arg = "", to_arg = "") {
+  stop_incompatible_cast(x, to, x_arg = x_arg, to_arg = to_arg)
+}
+
+
+# `exclude` from base ------------------------------------------------
+
+#' @export
+vec_proxy.exclude <- function(x, ...) {
+  x
+}
+#' @export
+vec_restore.exclude <- function(x, ...) {
+  structure(x, class = "exclude")
+}
+
+#' @export
+vec_ptype2.exclude.exclude <- function(x, y, ...) {
+  x
+}
+#' @export
+vec_ptype2.integer.exclude <- function(x, y, ...) {
+  x
+}
+#' @export
+vec_ptype2.exclude.integer <- function(x, y, ...) {
+  y
+}
+#' @export
+vec_ptype2.double.exclude <- function(x, y, ...) {
+  x
+}
+#' @export
+vec_ptype2.exclude.double <- function(x, y, ...) {
+  y
+}
+
+#' @export
+vec_cast.exclude.exclude <- function(x, to, ...) {
+  x
+}
+#' @export
+vec_cast.integer.exclude <- function(x, to, ...) {
+  vec_cast(vec_data(x), to, ...)
+}
+#' @export
+vec_cast.exclude.integer <- function(x, to, ..., x_arg = "", to_arg = "") {
+  stop_incompatible_cast(x, to, x_arg = x_arg, to_arg = to_arg)
+}
+#' @export
+vec_cast.double.exclude <- function(x, to, ...) {
+  vec_cast(vec_data(x), to, ...)
+}
+#' @export
+vec_cast.exclude.double <- function(x, to, ..., x_arg = "", to_arg = "") {
+  stop_incompatible_cast(x, to, x_arg = x_arg, to_arg = to_arg)
+}
+
+
 # `data.table` -------------------------------------------------------
 
 delayedAssign("as.data.table", {
