6eb3ba0b2dcac0537488206a7607e1b945ef8de3
Lionel Henry <lionel.hry@gmail.com>
2020-07-15 17:48:49 +0200

    Use `r_vec_deref_const()` in dictionary.c

--- src/utils.c
+++ src/utils.c
@@ -906,8 +906,12 @@ void* r_vec_deref(SEXP x) {
 
 const void* r_vec_const_deref(SEXP x) {
   switch (TYPEOF(x)) {
+  case LGLSXP: return LOGICAL_RO(x);
   case INTSXP: return INTEGER_RO(x);
+  case REALSXP: return REAL_RO(x);
+  case CPLXSXP: return COMPLEX_RO(x);
   case STRSXP: return STRING_PTR_RO(x);
+  case RAWSXP: return RAW_RO(x);
   default: stop_unimplemented_type("r_vec_const_deref", TYPEOF(x));
   }
 }
