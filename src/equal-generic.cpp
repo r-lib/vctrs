d4e02270202913807c68b82880568e23e1619780
Lionel Henry <lionel.hry@gmail.com>
2020-07-16 12:52:05 +0200

    Use templated functor for equality

--- src/equal-generic.cpp
+++ src/equal-generic.cpp
@@ -1,26 +1,39 @@
 #include "rlang.hpp"
 
 
-static inline
-int equal_scalar_na_equal(const int x, const int y) {
-  return x == y;
-}
-
-
 template <enum r_type R_TYPE,
           typename C_TYPE = DEFAULT_CTYPE(R_TYPE)>
+struct equal_scalar_na_equal {
+  int operator()(const C_TYPE x, const C_TYPE y) {
+    return x == y;
+  }
+};
+
+template <enum r_type R_TYPE,
+          typename C_TYPE = DEFAULT_CTYPE(R_TYPE),
+          typename EQ>
 static
-void equal_fill_na_equal(const void* p_x_payload,
-                         const void* p_y_payload,
-                         int (*eq)(const C_TYPE, const C_TYPE),
-                         r_ssize size,
-                         int* p_out) {
+void equal_fill_na_loop(const void* p_x_payload,
+                        const void* p_y_payload,
+                        EQ eq,
+                        r_ssize size,
+                        int* p_out) {
   const C_TYPE* p_x = (const C_TYPE*) p_x_payload;
   const C_TYPE* p_y = (const C_TYPE*) p_y_payload;
 
   for (r_ssize i = 0; i < size; ++i) {
     p_out[i] = eq(p_x[i], p_y[i]);
   }
+}
+
+template <enum r_type R_TYPE>
+static
+void equal_fill_na_equal(const void* p_x_payload,
+                         const void* p_y_payload,
+                         r_ssize size,
+                         int* p_out) {
+  auto eq = equal_scalar_na_equal<R_TYPE>();
+  equal_fill_na_loop<R_TYPE>(p_x_payload, p_y_payload, eq, size, p_out);
 }
 
 static
@@ -30,8 +43,8 @@ void equal_fill_na_equal(enum r_type type,
                          r_ssize size,
                          int* p_out) {
   switch (type) {
-  case r_type_logical: equal_fill_na_equal<r_type_logical>(p_x, p_y, &equal_scalar_na_equal, size, p_out); return;
-  case r_type_integer: equal_fill_na_equal<r_type_integer>(p_x, p_y, &equal_scalar_na_equal, size, p_out); return;
+  case r_type_logical: equal_fill_na_equal<r_type_logical>(p_x, p_y, size, p_out); return;
+  case r_type_integer: equal_fill_na_equal<r_type_integer>(p_x, p_y, size, p_out); return;
   default: throw;
   }
 }
