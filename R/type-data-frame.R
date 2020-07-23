fe1766ce380683b928dfe6ff3c78118f9b020ab9
DavisVaughan <davis@rstudio.com>
2020-07-23 12:07:26 -0400

    Document and export `data_frame()`

--- R/type-data-frame.R
+++ R/type-data-frame.R
@@ -27,7 +27,75 @@ new_data_frame <- function(x = list(), n = NULL, ..., class = NULL) {
 }
 new_data_frame <- fn_inline_formals(new_data_frame, "x")
 
-
+#' Construct a data frame
+#'
+#' @description
+#' `data_frame()` constructs a data frame. It is similar to
+#' [base::data.frame()], but there are a few notable differences that make it
+#' more in line with vctrs principles:
+#'
+#' - Inputs are recycled to a common size with [vec_recycle_common()].
+#'
+#' - With the exception of data frames, inputs are not modified in any way.
+#'   Character vectors are never converted to factors, and lists are stored
+#'   as-is for easy creation of list-columns.
+#'
+#' - Unnamed data frame inputs are automatically spliced. Named data frame
+#'   inputs are stored unmodified as data frame columns.
+#'
+#' - `NULL` inputs are completely ignored.
+#'
+#' - The dots are dynamic, allowing for splicing of lists with `!!!` and
+#'   unquoting.
+#'
+#' @details
+#' If no column names are supplied, `""` will be used as a default for all
+#' columns. This is applied before name repair occurs, so the default
+#' name repair of `"check_unique"` will error if any unnamed inputs
+#' are supplied and `"unique"` will repair the empty string column names
+#' appropriately. If the column names don't matter, use a `"minimal"` name
+#' repair for convenience and performance.
+#'
+#' @param ... Vectors to become columns in the data frame. When inputs are
+#'   named, those names are used for column names.
+#' @param .size The number of rows in the data frame. If `NULL`, this will
+#'   be computed as the common size of the inputs.
+#' @param .name_repair One of `"check_unique"`, `"unique"`, `"universal"` or
+#'   `"minimal"`. See [vec_as_names()] for the meaning of these options.
+#'
+#' @export
+#' @examples
+#' data_frame(x = 1, y = 2)
+#'
+#' # Inputs are recycled using tidyverse recycling rules
+#' data_frame(x = 1, y = 1:3)
+#'
+#' # Strings are never converted to factors
+#' class(data_frame(x = "foo")$x)
+#'
+#' # List columns can be easily created
+#' df <- data_frame(x = list(1:2, 2, 3:4), y = 3:1)
+#'
+#' # However, the base print method is suboptimal for displaying them,
+#' # so it is recommended to convert them to tibble
+#' if (rlang::is_installed("tibble")) {
+#'   tibble::as_tibble(df)
+#' }
+#'
+#' # Named data frame inputs create data frame columns
+#' df <- data_frame(x = data_frame(y = 1:2, z = "a"))
+#'
+#' # The `x` column itself is another data frame
+#' df$x
+#'
+#' # Again, it is recommended to convert these to tibbles for a better
+#' # print method
+#' if (rlang::is_installed("tibble")) {
+#'   tibble::as_tibble(df)
+#' }
+#'
+#' # Unnamed data frame input is automatically spliced
+#' data_frame(x = 1, data_frame(y = 1:2, z = "a"))
 data_frame <- function(...,
                        .size = NULL,
                        .name_repair = c("check_unique", "unique", "universal", "minimal")) {
