5f5ff4f910a38e7f02d9eeee9f64292850fbaa76
Lionel Henry <lionel.hry@gmail.com>
2020-08-19 16:08:28 +0200

    Extract list and atomic mapping in different functions

--- src/map.c
+++ src/map.c
@@ -5,6 +5,9 @@
 // Defined at load time
 SEXP vec_map_call = NULL;
 
+static SEXP atomic_map(SEXP x, SEXP env, SEXP ptype);
+static SEXP list_map(SEXP x, SEXP env, SEXP ptype);
+
 
 SEXP vctrs_map(SEXP args) {
     args = CDR(args);
@@ -12,78 +15,90 @@ SEXP vctrs_map(SEXP args) {
     SEXP env = CAR(args); args = CDR(args);
     SEXP ptype = CAR(args);
 
-    SEXP orig = x;
-    bool list_input = vec_is_list(orig);
-    bool list_output = vec_is_list(ptype);
-
-    if (list_input) {
-      x = PROTECT(x);
-    } else {
-      x = PROTECT(vec_chop2(x));
-    }
-
-    r_ssize n = r_length(x);
-    const SEXP* p_x = VECTOR_PTR_RO(x);
-
     if (ptype == R_NilValue) {
       r_abort("`.ptype` can't be NULL.");
     }
 
+    SEXP orig = x;
+    bool list_input = vec_is_list(orig);
+    bool list_output = vec_is_list(ptype);
+
+    if (!list_input) {
+      x = vec_chop2(x);
+    }
+    PROTECT(x);
+
     SEXP out;
-    SEXP out_proxy;
-
     if (list_output) {
-      out = PROTECT(r_clone_referenced(x));
-      SET_ATTRIB(out, R_NilValue);
-      SET_OBJECT(out, 0);
-      out_proxy = PROTECT(R_NilValue);
+      out = list_map(x, env, ptype);
     } else {
-      out = PROTECT(vec_init(ptype, n));
-      out_proxy = PROTECT(vec_proxy(out));
+      out = atomic_map(x, env, ptype);
     }
-
-    SEXP loc = PROTECT(compact_seq(0, 0, true));
-    int* p_loc = INTEGER(loc);
-
-    if (list_output) {
-      for (r_ssize i = 0; i < n; ++i) {
-        r_env_poke(env, syms_dot_elt, p_x[i]);
-        SET_VECTOR_ELT(out, i, r_eval_force(vec_map_call, env));
-      }
-
-      // Should use a ptype identity check before casting. Probably
-      // `vec_cast()` should make that check.
-      if (OBJECT(ptype)) {
-        out = vec_cast(out, ptype, NULL, NULL);
-      }
-      PROTECT(out);
-    } else {
-      PROTECT_INDEX out_proxy_pi;
-      PROTECT_WITH_INDEX(out_proxy, &out_proxy_pi);
-
-      for (r_ssize i = 0; i < n; ++i) {
-        r_env_poke(env, syms_dot_elt, p_x[i]);
-
-        SEXP elt_out = PROTECT(r_eval_force(vec_map_call, env));
-        elt_out = PROTECT(vec_cast(elt_out, ptype, NULL, NULL));
-
-        init_compact_seq(p_loc, i, 1, true);
-        out_proxy = vec_proxy_assign(out_proxy, loc, elt_out);
-
-        UNPROTECT(2);
-        REPROTECT(out_proxy, out_proxy_pi);
-      }
-
-      out = vec_restore(out_proxy, ptype, R_NilValue, VCTRS_OWNED_true);
-      UNPROTECT(1);
-      PROTECT(out);
-    }
+    PROTECT(out);
 
     SEXP names = PROTECT(vec_names(orig));
     vec_set_names(out, names);
 
-    UNPROTECT(6);
+    UNPROTECT(3);
     return out;
+}
+
+static
+SEXP list_map(SEXP x, SEXP env, SEXP ptype) {
+  SEXP out = PROTECT(r_clone_referenced(x));
+  SET_ATTRIB(out, R_NilValue);
+  SET_OBJECT(out, 0);
+
+  r_ssize n = r_length(x);
+  const SEXP* p_x = VECTOR_PTR_RO(x);
+
+  for (r_ssize i = 0; i < n; ++i) {
+    r_env_poke(env, syms_dot_elt, p_x[i]);
+    SET_VECTOR_ELT(out, i, r_eval_force(vec_map_call, env));
+  }
+
+  // Should use a ptype identity check before casting. Probably
+  // `vec_cast()` should make that check.
+  if (OBJECT(ptype)) {
+    out = vec_cast(out, ptype, NULL, NULL);
+  }
+
+  UNPROTECT(1);
+  return out;
+}
+
+static
+SEXP atomic_map(SEXP x, SEXP env, SEXP ptype) {
+  r_ssize n = r_length(x);
+
+  SEXP out = PROTECT(vec_init(ptype, n));
+
+  SEXP out_proxy = vec_proxy(out);
+  PROTECT_INDEX out_proxy_pi;
+  PROTECT_WITH_INDEX(out_proxy, &out_proxy_pi);
+
+  SEXP loc = PROTECT(compact_seq(0, 0, true));
+  int* p_loc = INTEGER(loc);
+
+  const SEXP* p_x = VECTOR_PTR_RO(x);
+
+  for (r_ssize i = 0; i < n; ++i) {
+    r_env_poke(env, syms_dot_elt, p_x[i]);
+
+    SEXP elt_out = PROTECT(r_eval_force(vec_map_call, env));
+    elt_out = PROTECT(vec_cast(elt_out, ptype, NULL, NULL));
+
+    init_compact_seq(p_loc, i, 1, true);
+    out_proxy = vec_proxy_assign(out_proxy, loc, elt_out);
+
+    UNPROTECT(2);
+    REPROTECT(out_proxy, out_proxy_pi);
+  }
+
+  out = vec_restore(out_proxy, ptype, R_NilValue, VCTRS_OWNED_true);
+
+  UNPROTECT(3);
+  return out;
 }
 
 
