21f48f1a436d4c402db0201adc5a395406fd4eda
Lionel Henry <lionel.hry@gmail.com>
2020-08-26 18:25:09 +0200

    Use Waldo expectations

--- tests/testthat/helper-vctrs.R
+++ tests/testthat/helper-vctrs.R
@@ -32,3 +32,35 @@ set_rownames_recursively <- function(x, i = NULL) {
   row.names(x) <- paste0(letters[seq_len(n)], i)
   x
 }
+
+expect_waldo_equal <- function(type, act, exp, info, ...) {
+  comp <- waldo::compare(act$val, exp$val, ..., x_arg = "actual", y_arg = "expected")
+  expect(
+    length(comp) == 0,
+    sprintf(
+      "`actual` (%s) not %s to `expected` (%s).\n\n%s",
+      act$lab, type, exp$lab, paste0(comp, collapse = "\n\n")
+    ),
+    info = info
+  )
+
+  invisible(act$val)
+}
+
+expect_identical <- function(object, expected, info = NULL, label = NULL,
+                             expected.label = NULL, ...) {
+  act <- quasi_label(enquo(object), label, arg = "object")
+  exp <- quasi_label(enquo(expected), expected.label, arg = "expected")
+
+  expect_waldo_equal("identical", act, exp, info, ...)
+}
+
+expect_equal <- function(object, expected, ...,
+                         tolerance = .Machine$double.eps ^ 0.5,
+                         info = NULL, label = NULL,
+                         expected.label = NULL) {
+  act <- quasi_label(enquo(object), label, arg = "object")
+  exp <- quasi_label(enquo(expected), expected.label, arg = "expected")
+
+  expect_waldo_equal("equal", act, exp, info, ..., tolerance = tolerance)
+}
