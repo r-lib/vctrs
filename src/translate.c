58ba3e8c952d100ef0d62e46675628072aadc2db
DavisVaughan <davis@rstudio.com>
2020-09-01 10:53:30 -0400

    Performance - no need to set unchanged element

--- src/translate.c
+++ src/translate.c
@@ -220,7 +220,6 @@ static SEXP chr_translate_encoding(SEXP x, r_ssize size) {
     SEXP elt = p_x[i];
 
     if (Rf_getCharCE(elt) == CE_UTF8) {
-      SET_STRING_ELT(out, i, elt);
       continue;
     }
 
