14e52d0c0bd285a230e720cf0d4ef1c8de096033
DavisVaughan <davis@rstudio.com>
2020-09-04 16:51:39 -0400

    Add tests, document, and export `vec_identify_runs()`

--- R/runs.R
+++ R/runs.R
@@ -1,3 +1,36 @@
+#' Runs
+#'
+#' @description
+#' - `vec_identify_runs()` returns an identifier for the run of repeated values
+#'   that each element of `x` falls in. The number of runs is also returned as
+#'   an attribute, `n`.
+#'
+#' @details
+#' Unlike [base::rle()], adjacent missing values are considered identical when
+#' constructing runs. For example, `vec_identify_runs(c(NA, NA))` will return
+#' `c(1, 1)`, not `c(1, 2)`.
+#'
+#' @param x A vector.
+#'
+#' @return
+#' - `vec_identify_runs()`: An integer vector with the same size as `x`. A
+#'   scalar integer attribute, `n`, is attached.
+#'
+#' @export
+#' @examples
+#' x <- c("a", "z", "z", "c", "a", "a")
+#'
+#' vec_identify_runs(x)
+#'
+#' y <- c(1, 1, 1, 2, 2, 3)
+#'
+#' # With multiple columns, the runs are constructed rowwise
+#' df <- data_frame(
+#'   x = x,
+#'   y = y
+#' )
+#'
+#' vec_identify_runs(df)
 vec_identify_runs <- function(x) {
   .Call(vctrs_identify_runs, x)
 }
