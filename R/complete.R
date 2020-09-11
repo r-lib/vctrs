0a77c0adffe95e4f8fd9b53d1fc8bd8fdf435429
DavisVaughan <davis@rstudio.com>
2020-09-11 14:39:48 -0400

    Update documentation to reflect that this is for data frames

--- R/complete.R
+++ R/complete.R
@@ -1,11 +1,10 @@
 #' Complete
 #'
 #' @description
-#' These functions are for working with "complete" values. For atomic vectors,
-#' this is the same as `!vec_equal_na(x)`. For data frames, a row is only
-#' considered complete if all elements of that row are non-missing.
-#' To compare, `!vec_equal_na(x)` for data frames detects rows that have at
-#' least one non-missing value.
+#' These functions are for working with "complete" rows of a data frame. A
+#' row is only considered complete if all elements of that row are non-missing.
+#' To compare, `!vec_equal_na(x)` detects rows that have at least one
+#' non-missing value.
 #'
 #' - `df_slice_complete()` returns a vector with the same type as `x` holding
 #'   the complete elements.
@@ -16,22 +15,25 @@
 #' - `df_detect_complete()` returns a logical vector that detects if elements
 #'   of `x` are complete.
 #'
-#' @param x A vector
+#' @param x A data frame
 #'
 #' @return
 #' - `df_slice_complete()`: A vector with the same type as `x`. The size
 #'   of the result is equal to the number of complete elements.
 #'
-#' - `df_locate_complete()` An integer vector. The size
+#' - `df_locate_complete()`: An integer vector. The size
 #'   of the result is equal to the number of complete elements.
 #'
-#' - `df_slice_complete()` A logical vector with the same size as `x`.
+#' - `df_detect_complete()`: A logical vector with the same size as `x`.
 #'
-#' @name vec-complete
+#' @name df-complete
 #' @seealso [stats::complete.cases()]
 #' @export
 #' @examples
-#' df <- data_frame(x = c(1, 2, NA, 4, NA), y = c("a", "b", NA, "d", "e"))
+#' df <- data_frame(
+#'   x = c(1, 2, NA, 4, NA),
+#'   y = c("a", "b", NA, "d", "e")
+#' )
 #'
 #' # This returns `TRUE` where all elements of the row are non-missing.
 #' # Compare that with `!vec_equal_na()`, which detects rows that have at
@@ -47,13 +49,13 @@ df_slice_complete <- function(x) {
   .Call(vctrs_df_slice_complete, x)
 }
 
-#' @rdname vec-complete
+#' @rdname df-complete
 #' @export
 df_locate_complete <- function(x) {
   .Call(vctrs_df_locate_complete, x)
 }
 
-#' @rdname vec-complete
+#' @rdname df-complete
 #' @export
 df_detect_complete <- function(x) {
   .Call(vctrs_df_detect_complete, x)
