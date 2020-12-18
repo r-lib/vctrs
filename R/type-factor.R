3a1015f65416b4ed63ddd9a7f53d493c802df4e9
DavisVaughan <davis@rstudio.com>
2020-12-18 08:58:19 -0500

    Replace `digest::digest()` with `rlang::hash()`

--- R/type-factor.R
+++ R/type-factor.R
@@ -225,7 +225,7 @@ hash_label <- function(x, length = 5) {
   } else {
     # Can't use hash() currently because it hashes the string pointers
     # for performance, so the values in the test change each time
-    substr(digest::digest(x), 1, length)
+    substr(rlang::hash(x), 1, length)
   }
 }
 
