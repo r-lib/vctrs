33ba3a3ae6296c1d3ca7e8fc9128beb65bfab8e1
DavisVaughan <davis@rstudio.com>
2020-12-18 09:01:10 -0500

    Update comment to use the correct vctrs function name

--- R/type-factor.R
+++ R/type-factor.R
@@ -223,7 +223,7 @@ hash_label <- function(x, length = 5) {
   if (length(x) == 0) {
     ""
   } else {
-    # Can't use hash() currently because it hashes the string pointers
+    # Can't use obj_hash() because it hashes the string pointers
     # for performance, so the values in the test change each time
     substr(rlang::hash(x), 1, length)
   }
