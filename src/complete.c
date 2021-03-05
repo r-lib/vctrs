6a1a29cf2caa2ac26dca8db487ca5e37995093da
DavisVaughan <davis@rstudio.com>
2021-03-05 13:07:32 -0500

    Rework C side to allow all vector types

--- src/complete.c
+++ src/complete.c
@@ -48,14 +48,10 @@ SEXP vctrs_detect_complete(SEXP x) {
   return vec_detect_complete(x);
 }
 
-static inline void vec_detect_complete_col(SEXP x, R_len_t size, int* p_out);
+static inline void vec_detect_complete_switch(SEXP x, R_len_t size, int* p_out);
 
 static
 SEXP vec_detect_complete(SEXP x) {
-  if (!is_data_frame(x)) {
-    r_abort("`x` must be a data frame.");
-  }
-
   SEXP proxy = PROTECT(vec_proxy_complete(x));
 
   R_len_t size = vec_size(proxy);
@@ -68,19 +64,7 @@ SEXP vec_detect_complete(SEXP x) {
     p_out[i] = 1;
   }
 
-  // Proxy unwrapping might have flattened `x`
-  if (!is_data_frame(proxy)) {
-    vec_detect_complete_col(proxy, size, p_out);
-    UNPROTECT(2);
-    return out;
-  }
-
-  r_ssize n_cols = r_length(proxy);
-  const SEXP* p_proxy = VECTOR_PTR_RO(proxy);
-
-  for (r_ssize i = 0; i < n_cols; ++i) {
-    vec_detect_complete_col(p_proxy[i], size, p_out);
-  }
+  vec_detect_complete_switch(proxy, size, p_out);
 
   UNPROTECT(2);
   return out;
@@ -88,27 +72,28 @@ SEXP vec_detect_complete(SEXP x) {
 
 // -----------------------------------------------------------------------------
 
-static inline void lgl_detect_complete_col(SEXP x, R_len_t size, int* p_out);
-static inline void int_detect_complete_col(SEXP x, R_len_t size, int* p_out);
-static inline void dbl_detect_complete_col(SEXP x, R_len_t size, int* p_out);
-static inline void cpl_detect_complete_col(SEXP x, R_len_t size, int* p_out);
-static inline void chr_detect_complete_col(SEXP x, R_len_t size, int* p_out);
-static inline void raw_detect_complete_col(SEXP x, R_len_t size, int* p_out);
-static inline void list_detect_complete_col(SEXP x, R_len_t size, int* p_out);
+static inline void lgl_detect_complete(SEXP x, R_len_t size, int* p_out);
+static inline void int_detect_complete(SEXP x, R_len_t size, int* p_out);
+static inline void dbl_detect_complete(SEXP x, R_len_t size, int* p_out);
+static inline void cpl_detect_complete(SEXP x, R_len_t size, int* p_out);
+static inline void chr_detect_complete(SEXP x, R_len_t size, int* p_out);
+static inline void raw_detect_complete(SEXP x, R_len_t size, int* p_out);
+static inline void list_detect_complete(SEXP x, R_len_t size, int* p_out);
+static inline void df_detect_complete(SEXP x, R_len_t size, int* p_out);
 
 static inline
-void vec_detect_complete_col(SEXP x, R_len_t size, int* p_out) {
+void vec_detect_complete_switch(SEXP x, R_len_t size, int* p_out) {
   switch (vec_proxy_typeof(x)) {
-  case vctrs_type_logical: lgl_detect_complete_col(x, size, p_out); break;
-  case vctrs_type_integer: int_detect_complete_col(x, size, p_out); break;
-  case vctrs_type_double: dbl_detect_complete_col(x, size, p_out); break;
-  case vctrs_type_complex: cpl_detect_complete_col(x, size, p_out); break;
-  case vctrs_type_character: chr_detect_complete_col(x, size, p_out); break;
-  case vctrs_type_raw: raw_detect_complete_col(x, size, p_out); break;
-  case vctrs_type_list: list_detect_complete_col(x, size, p_out); break;
-  case vctrs_type_dataframe: stop_internal("vec_detect_complete_col", "Data frame columns should have been flattened by now.");
-  case vctrs_type_scalar: stop_internal("vec_detect_complete_col", "Can't detect missing values in scalars.");
-  default: stop_unimplemented_vctrs_type("vec_detect_complete_col", vec_proxy_typeof(x));
+  case vctrs_type_logical: lgl_detect_complete(x, size, p_out); break;
+  case vctrs_type_integer: int_detect_complete(x, size, p_out); break;
+  case vctrs_type_double: dbl_detect_complete(x, size, p_out); break;
+  case vctrs_type_complex: cpl_detect_complete(x, size, p_out); break;
+  case vctrs_type_character: chr_detect_complete(x, size, p_out); break;
+  case vctrs_type_raw: raw_detect_complete(x, size, p_out); break;
+  case vctrs_type_list: list_detect_complete(x, size, p_out); break;
+  case vctrs_type_dataframe: df_detect_complete(x, size, p_out); break;
+  case vctrs_type_scalar: stop_internal("vec_detect_complete", "Can't detect missing values in scalars.");
+  default: stop_unimplemented_vctrs_type("vec_detect_complete", vec_proxy_typeof(x));
   }
 }
 
@@ -142,43 +127,57 @@ void vec_detect_complete_col(SEXP x, R_len_t size, int* p_out) {
  * ```
  */
 
-#define VEC_DETECT_COMPLETE_COL(CTYPE, CONST_DEREF, IS_MISSING) { \
-  const CTYPE* p_x = CONST_DEREF(x);                              \
-                                                                  \
-  for (R_len_t i = 0; i < size; ++i) {                            \
-    const CTYPE elt = p_x[i];                                     \
-                                                                  \
-    if (IS_MISSING(elt)) {                                        \
-      p_out[i] = 0;                                               \
-    }                                                             \
-  }                                                               \
+#define VEC_DETECT_COMPLETE(CTYPE, CONST_DEREF, IS_MISSING) { \
+  const CTYPE* p_x = CONST_DEREF(x);                          \
+                                                              \
+  for (R_len_t i = 0; i < size; ++i) {                        \
+    const CTYPE elt = p_x[i];                                 \
+                                                              \
+    if (IS_MISSING(elt)) {                                    \
+      p_out[i] = 0;                                           \
+    }                                                         \
+  }                                                           \
 }
 
 static inline
-void lgl_detect_complete_col(SEXP x, R_len_t size, int* p_out) {
-  VEC_DETECT_COMPLETE_COL(int, LOGICAL_RO, lgl_is_missing);
+void lgl_detect_complete(SEXP x, R_len_t size, int* p_out) {
+  VEC_DETECT_COMPLETE(int, LOGICAL_RO, lgl_is_missing);
 }
 static inline
-void int_detect_complete_col(SEXP x, R_len_t size, int* p_out) {
-  VEC_DETECT_COMPLETE_COL(int, INTEGER_RO, int_is_missing);
+void int_detect_complete(SEXP x, R_len_t size, int* p_out) {
+  VEC_DETECT_COMPLETE(int, INTEGER_RO, int_is_missing);
 }
 static inline
-void dbl_detect_complete_col(SEXP x, R_len_t size, int* p_out) {
-  VEC_DETECT_COMPLETE_COL(double, REAL_RO, dbl_is_missing);
+void dbl_detect_complete(SEXP x, R_len_t size, int* p_out) {
+  VEC_DETECT_COMPLETE(double, REAL_RO, dbl_is_missing);
 }
 static inline
-void cpl_detect_complete_col(SEXP x, R_len_t size, int* p_out) {
-  VEC_DETECT_COMPLETE_COL(Rcomplex, COMPLEX_RO, cpl_is_missing);
+void cpl_detect_complete(SEXP x, R_len_t size, int* p_out) {
+  VEC_DETECT_COMPLETE(Rcomplex, COMPLEX_RO, cpl_is_missing);
 }
 static inline
-void chr_detect_complete_col(SEXP x, R_len_t size, int* p_out) {
-  VEC_DETECT_COMPLETE_COL(SEXP, STRING_PTR_RO, chr_is_missing);
+void chr_detect_complete(SEXP x, R_len_t size, int* p_out) {
+  VEC_DETECT_COMPLETE(SEXP, STRING_PTR_RO, chr_is_missing);
 }
 static inline
-void raw_detect_complete_col(SEXP x, R_len_t size, int* p_out) {
-  VEC_DETECT_COMPLETE_COL(Rbyte, RAW_RO, raw_is_missing);
+void raw_detect_complete(SEXP x, R_len_t size, int* p_out) {
+  VEC_DETECT_COMPLETE(Rbyte, RAW_RO, raw_is_missing);
 }
 static inline
-void list_detect_complete_col(SEXP x, R_len_t size, int* p_out) {
-  VEC_DETECT_COMPLETE_COL(SEXP, VECTOR_PTR_RO, list_is_missing);
+void list_detect_complete(SEXP x, R_len_t size, int* p_out) {
+  VEC_DETECT_COMPLETE(SEXP, VECTOR_PTR_RO, list_is_missing);
+}
+
+#undef VEC_DETECT_COMPLETE
+
+// -----------------------------------------------------------------------------
+
+static inline
+void df_detect_complete(SEXP x, R_len_t size, int* p_out) {
+  r_ssize n_cols = r_length(x);
+  const SEXP* p_x = VECTOR_PTR_RO(x);
+
+  for (r_ssize i = 0; i < n_cols; ++i) {
+    vec_detect_complete_switch(p_x[i], size, p_out);
+  }
 }
