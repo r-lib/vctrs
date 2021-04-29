928bcf0ae02c9eeaa6ee022d1a19399e34cec2ea
Lionel Henry <lionel.hry@gmail.com>
2021-04-29 16:40:12 +0200

    Use rlang's exported version if available

--- R/aaa.R
+++ R/aaa.R
@@ -1,3 +1,31 @@
+on_load <- function(expr, env = parent.frame()) {
+  ns <- topenv(env)
+  expr <- substitute(expr)
+  callback <- function() eval_bare(expr, env)
+  ns$.__rlang_hook__. <- c(ns$.__rlang_hook__., list(callback))
+}
+
+run_on_load <- function(env = parent.frame()) {
+  ns <- topenv(env)
+
+  hook <- ns$.__rlang_hook__.
+  env_unbind(ns, ".__rlang_hook__.")
+
+  for (callback in hook) {
+    callback()
+  }
+
+  ns$.__rlang_hook__. <- NULL
+}
+
+replace_from <- function(what, pkg, to = topenv(caller_env())) {
+  if (what %in% getNamespaceExports(pkg)) {
+    env <- ns_env(pkg)
+  } else {
+    env <- to
+  }
+  env_get(env, what, inherit = TRUE)
+}
 
 # nocov start
 
