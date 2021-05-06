e406f6bb05004f2bc60edd65ee7907186f8f8ca3
DavisVaughan <davis@rstudio.com>
2021-05-06 16:44:50 -0400

    Add a custom `as.data.frame.array()` that works with 1-D arrays
    This is also different from `as.data.frame2()`, in that each column of the result has the opportunity to retain the input class.

--- R/type-integer64.R
+++ R/type-integer64.R
@@ -3,7 +3,7 @@ vec_proxy_equal.integer64 <- function(x, ...) {
   if (is.array(x)) {
     # Stopgap to convert arrays to data frames, then run them through
     # `vec_proxy_equal()` again, which will proxy each column
-    x <- as.data.frame(x)
+    x <- as_data_frame_from_array(x)
     x <- vec_proxy_equal(x)
     return(x)
   }
@@ -124,3 +124,37 @@ integer64_proxy <- function(x) {
 integer64_restore <- function(x) {
   .Call(vctrs_integer64_restore, x)
 }
+
+# ------------------------------------------------------------------------------
+
+as_data_frame_from_array <- function(x) {
+  # Alternative to `as.data.frame.array()` that always strips 1-D arrays
+  # of their dimensions. Unlike `as.data.frame2()`, it doesn't unclass the
+  # input, which means that each column retains its original class.
+  # This function doesn't attempt to keep the names of `x` at all.
+
+  dim <- dim(x)
+  n_dim <- length(dim)
+
+  if (n_dim == 1) {
+    # Treat 1-D arrays as 1 column matrices
+    dim(x) <- c(dim, 1L)
+    n_dim <- 2L
+  }
+
+  n_row <- dim[[1L]]
+  n_col <- prod(dim[-1L])
+  n_col_seq <- seq_len(n_col)
+
+  dim(x) <- c(n_row, n_col)
+
+  out <- vector("list", n_col)
+  names(out) <- as_unique_names(rep("", n_col), quiet = TRUE)
+
+  for (i in n_col_seq) {
+    out[[i]] <- x[, i, drop = TRUE]
+  }
+
+  new_data_frame(out, n = n_row)
+}
+
