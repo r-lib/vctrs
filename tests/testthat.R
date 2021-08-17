6a14d8ec74a8ba10b04767a7709a290ff847c689
DavisVaughan <davis@rstudio.com>
2021-08-17 13:51:12 -0400

    Switch to testthat 3e

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,4 @@
 library(testthat)
 library(vctrs)
 
-if (requireNamespace("xml2")) {
-  test_check("vctrs", reporter = MultiReporter$new(reporters = list(JunitReporter$new(file = "test-results.xml"), CheckReporter$new())))
-} else {
-  test_check("vctrs")
-}
+test_check("vctrs")
