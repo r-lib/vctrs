a65b7d336de013b7b71838b0306511be0e7b6ccf
DavisVaughan <davis@rstudio.com>
2022-08-31 17:15:31 -0400

    Test that `vec_unchop()` still works and is deprecated

--- tests/testthat/test-lifecycle-deprecated.R
+++ tests/testthat/test-lifecycle-deprecated.R
@@ -13,3 +13,16 @@ test_that("vec_repeat() still works", {
   expect_identical(vec_repeat(1:2, times = 2), vec_rep(1:2, 2))
   expect_identical(vec_repeat(1:2, each = 2), vec_rep_each(1:2, 2))
 })
+
+test_that("vec_unchop() is soft-deprecated", {
+  local_lifecycle_warnings()
+  expect_snapshot(vec_unchop(list(1), indices = list(1)))
+})
+
+test_that("vec_unchop() still works", {
+  local_lifecycle_silence()
+  expect_identical(
+    vec_unchop(list(1L, 2:3), indices = list(2, c(3, 1))),
+    c(3L, 1L, 2L)
+  )
+})
