4622979ef0cb7ec8cacdd69928588d4b6d69b32a
Lionel Henry <lionel.hry@gmail.com>
2022-09-27 17:03:38 +0200

    Remove logo from package topic

--- R/vctrs-package.R
+++ R/vctrs-package.R
@@ -1,7 +1,4 @@
 #' @description
-#' \if{html}{\figure{logo.png}{options: align='right'}}
-#' `r lifecycle::badge("maturing")`
-#'
 #' Defines new notions of prototype and size that are
 #' used to provide tools for consistent and well-founded type-coercion
 #' and size-recycling, and are in turn connected to ideas of type- and
