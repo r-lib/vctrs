af4652b3851f93790500972e474c5413dc4ee0e4
Lionel Henry <lionel.hry@gmail.com>
2022-09-28 17:12:15 +0200

    Add missing `s3_register()` helper

--- R/register-s3.R
+++ R/register-s3.R
@@ -125,6 +125,57 @@ s3_register <- function(generic, class, method = NULL) {
 
   invisible()
 }
+
+.rlang_s3_register_compat <- function(fn, try_rlang = TRUE) {
+  # Compats that behave the same independently of rlang's presence
+  out <- switch(
+    fn,
+    is_installed = return(function(pkg) requireNamespace(pkg, quietly = TRUE))
+  )
+
+  # Only use rlang if it is fully loaded (#1482)
+  if (try_rlang &&
+        requireNamespace("rlang", quietly = TRUE) &&
+        environmentIsLocked(asNamespace("rlang"))) {
+    switch(
+      fn,
+      is_interactive = return(rlang::is_interactive)
+    )
+
+    # Make sure rlang knows about "x" and "i" bullets
+    if (utils::packageVersion("rlang") >= "0.4.2") {
+      switch(
+        fn,
+        abort = return(rlang::abort),
+        warn = return((rlang::warn)),
+        inform = return(rlang::inform)
+      )
+    }
+  }
+
+  # Fall back to base compats
+
+  is_interactive_compat <- function() {
+    opt <- getOption("rlang_interactive")
+    if (!is.null(opt)) {
+      opt
+    } else {
+      interactive()
+    }
+  }
+
+  format_msg <- function(x) paste(x, collapse = "\n")
+  switch(
+    fn,
+    is_interactive = return(is_interactive_compat),
+    abort = return(function(msg) stop(format_msg(msg), call. = FALSE)),
+    warn = return(function(msg) warning(format_msg(msg), call. = FALSE)),
+    inform = return(function(msg) message(format_msg(msg)))
+  )
+
+  stop(sprintf("Internal error in rlang shims: Unknown function `%s()`.", fn))
+}
+
 on_load({
   s3_register <- replace_from("s3_register", "rlang")
 })
