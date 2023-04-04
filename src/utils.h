3f0d9b1dbd6a5767e9401f0dff764b49acdaf92a
Michael Chirico <michaelchirico4@gmail.com>
2023-04-03 19:51:01 -0700

    rename class->cls in utils.{c,h}

--- src/utils.h
+++ src/utils.h
@@ -134,10 +134,10 @@ SEXP chr_resize(SEXP x, r_ssize x_size, r_ssize size);
 
 // Returns S3 / S4 method for `generic` suitable for the class of `x`. The
 // inheritance hierarchy is explored except for the default method.
-SEXP s3_get_method(const char* generic, const char* class, SEXP table);
+SEXP s3_get_method(const char* generic, const char* cls, SEXP table);
 SEXP s3_sym_get_method(SEXP sym, SEXP table);
 SEXP s3_find_method(const char* generic, SEXP x, SEXP table);
-SEXP s3_class_find_method(const char* generic, SEXP class, SEXP table);
+SEXP s3_class_find_method(const char* generic, SEXP cls, SEXP table);
 SEXP s3_get_class(SEXP x);
 SEXP s3_find_method_xy(const char* generic,
                        SEXP x,
@@ -148,10 +148,10 @@ SEXP s3_find_method2(const char* generic,
                      SEXP x,
                      SEXP table,
                      SEXP* method_sym_out);
-SEXP s3_paste_method_sym(const char* generic, const char* class);
+SEXP s3_paste_method_sym(const char* generic, const char* cls);
 SEXP s3_bare_class(SEXP x);
 SEXP s4_find_method(SEXP x, SEXP table);
-SEXP s4_class_find_method(SEXP class, SEXP table);
+SEXP s4_class_find_method(SEXP cls, SEXP table);
 bool vec_implements_ptype2(SEXP x);
 
 SEXP r_env_get(SEXP env, SEXP sym);
