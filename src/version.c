0e17cb40b9817c738fa9eb68acf0070d46de08aa
DavisVaughan <davis@rstudio.com>
2023-04-19 09:33:12 -0400

    Increment version number to 0.6.2

--- src/version.c
+++ src/version.c
@@ -1,7 +1,7 @@
 #define R_NO_REMAP
 #include <Rinternals.h>
 
-const char* vctrs_version = "0.6.1.9000";
+const char* vctrs_version = "0.6.2";
 
 /**
  * This file records the expected package version in the shared
