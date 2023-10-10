360af4437bf599059752cd7f48c798d21ce59855
Davis Vaughan <davis@posit.co>
2023-10-10 15:57:15 -0400

    Make vector recycling rules its own FAQ

--- R/faq-developer.R
+++ R/faq-developer.R
@@ -12,6 +12,18 @@ NULL
 #' @name theory-faq-coercion
 NULL
 
+# The `@name` must be `vector_recycling_rules` to ensure a pkgdown page
+# continues to exist for `vector_recycling_rules` since other packages link to
+# this page and currently `@aliases` don't get a page on the pkgdown
+# site due to: https://github.com/r-lib/pkgdown/issues/1876
+#' FAQ - How does recycling work in vctrs and the tidyverse?
+#'
+#' @includeRmd man/faq/developer/theory-recycling.Rmd description
+#'
+#' @name vector_recycling_rules
+#' @aliases theory-faq-recycling
+NULL
+
 #' FAQ - How to implement ptype2 and cast methods?
 #'
 #' @includeRmd man/faq/developer/howto-coercion.Rmd description
