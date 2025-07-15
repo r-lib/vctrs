08d706c1606427bf3644e75a143d4150ed864de6
Davis Vaughan <davis@posit.co>
2025-07-15 10:50:42 -0400

    Just use correct link

--- R/type-rcrd.R
+++ R/type-rcrd.R
@@ -11,9 +11,9 @@
 #' @details
 #' Record-style objects created with [new_rcrd()] do not do much on their own.
 #' For instance they do not have a default [format()] method, which means
-#' printing the object causes an error. See the *Record-style objects* section
-#' from `vignette("s3-vector")` for details on implementing methods for
-#' record vectors.
+#' printing the object causes an error. See [Record-style
+#' objects](https://vctrs.r-lib.org/articles/s3-vector.html#record-style-objects)
+#' for details on implementing methods for record vectors.
 #'
 #' @param fields A list or a data frame. Lists must be rectangular
 #'   (same sizes), and contain uniquely named vectors (at least
