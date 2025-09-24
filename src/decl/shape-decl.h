8adffdf5df36c002feb0317285c61d108b9f711e
Davis Vaughan <davis@rstudio.com>
2025-09-24 11:09:43 -0400

    Avoid `KEEP()` and `FREE()` in `vec_shaped_ptype()` (#2044)
    * Avoid `KEEP()` and `FREE()` in `vec_shaped_ptype()`
    
    * NEWS
    
    * Expand on comment

--- src/decl/shape-decl.h
+++ src/decl/shape-decl.h
@@ -1,19 +1,31 @@
-static
-r_obj* vec_shape2(r_obj* x, r_obj* y, struct vctrs_arg* p_x_arg, struct vctrs_arg* p_y_arg);
-
-static
-r_obj* vec_shape2_impl(r_obj* x_dimensions,
-                       r_obj* y_dimensions,
-                       r_obj* x,
-                       r_obj* y,
-                       struct vctrs_arg* p_x_arg,
-                       struct vctrs_arg* p_y_arg);
-
-static
-r_obj* vec_shape(r_obj* dimensions);
+static inline
+r_obj* vec_shape2(
+  r_obj* x,
+  r_obj* y,
+  struct vctrs_arg* p_x_arg,
+  struct vctrs_arg* p_y_arg
+);
 
 static inline
-int vec_dimension2(int x_dimension, int y_dimension,
-                   int axis,
-                   r_obj* x, r_obj* y,
-                   struct vctrs_arg* p_x_arg, struct vctrs_arg* p_y_arg);
+r_obj* dims_shape(r_obj* dimensions);
+
+static inline
+r_obj* dims_shape2(
+  r_obj* x_dimensions,
+  r_obj* y_dimensions,
+  r_obj* x,
+  r_obj* y,
+  struct vctrs_arg* p_x_arg,
+  struct vctrs_arg* p_y_arg
+);
+
+static inline
+int dim2(
+  int x_dimension,
+  int y_dimension,
+  int axis,
+  r_obj* x,
+  r_obj* y,
+  struct vctrs_arg* p_x_arg,
+  struct vctrs_arg* p_y_arg
+);
--- src/shape.h
+++ src/shape.h
@@ -4,10 +4,16 @@
 #include "vctrs-core.h"
 #include "cast.h"
 
-
-SEXP vec_shaped_ptype(SEXP ptype,
-                      SEXP x, SEXP y,
-                      struct vctrs_arg* p_x_arg, struct vctrs_arg* p_y_arg);
+// Computes the common shape of `x` and `y` and attaches it as the
+// dimensions of `ptype`. If `x` and `y` are both atomic with `NULL` dimensions,
+// then no dimensions are attached and `ptype` is returned unmodified.
+r_obj* vec_shaped_ptype(
+  r_obj* ptype,
+  r_obj* x,
+  r_obj* y,
+  struct vctrs_arg* p_x_arg,
+  struct vctrs_arg* p_y_arg
+);
 
 r_obj* vec_shape_broadcast(r_obj* out, const struct cast_opts* p_opts);
 
