7e88ee5aa583115d738182d966b0408fb713ad9c
Davis Vaughan <davis@posit.co>
2025-09-24 10:14:15 -0400

    Avoid `KEEP()` and `FREE()` in `vec_shaped_ptype()`

--- src/shape.c
+++ src/shape.c
@@ -1,28 +1,6 @@
 #include "vctrs.h"
 #include "decl/shape-decl.h"
 
-
-// Computes the common shape of `x` and `y` and attaches it as the
-// dimensions of `ptype`. If `x` and `y` are both atomic with `NULL` dimensions,
-// then no dimensions are attached and `ptype` is returned unmodified.
-// [[ include("shape.h") ]]
-r_obj* vec_shaped_ptype(r_obj* ptype,
-                        r_obj* x, r_obj* y,
-                        struct vctrs_arg* p_x_arg, struct vctrs_arg* p_y_arg) {
-  r_obj* ptype_dimensions = KEEP(vec_shape2(x, y, p_x_arg, p_y_arg));
-
-  if (ptype_dimensions == r_null) {
-    FREE(1);
-    return ptype;
-  }
-
-  ptype = KEEP(r_clone_referenced(ptype));
-  r_attrib_poke_dim(ptype, ptype_dimensions);
-
-  FREE(2);
-  return ptype;
-}
-
 r_obj* ffi_vec_shaped_ptype(r_obj* ptype, r_obj* x, r_obj* y, r_obj* frame) {
   struct r_lazy x_arg_ = { .x = syms.x_arg, .env = frame };
   struct vctrs_arg x_arg = new_lazy_arg(&x_arg_);
@@ -33,22 +11,30 @@ r_obj* ffi_vec_shaped_ptype(r_obj* ptype, r_obj* x, r_obj* y, r_obj* frame) {
   return vec_shaped_ptype(ptype, x, y, &x_arg, &y_arg);
 }
 
+r_obj* vec_shaped_ptype(
+  r_obj* ptype,
+  r_obj* x,
+  r_obj* y,
+  struct vctrs_arg* p_x_arg,
+  struct vctrs_arg* p_y_arg
+) {
+  r_obj* shape = vec_shape2(x, y, p_x_arg, p_y_arg);
 
-// -----------------------------------------------------------------------------
+  if (shape == r_null) {
+    return ptype;
+  }
 
-static
-r_obj* vec_shape2(r_obj* x,
-                  r_obj* y,
-                  struct vctrs_arg* p_x_arg,
-                  struct vctrs_arg* p_y_arg) {
-  r_obj* x_dimensions = KEEP(r_dim(x));
-  r_obj* y_dimensions = KEEP(r_dim(y));
+  // Only `KEEP()` if we have to
+  KEEP(shape);
 
-  r_obj* out = vec_shape2_impl(x_dimensions, y_dimensions, x, y, p_x_arg, p_y_arg);
+  ptype = KEEP(r_clone_referenced(ptype));
+  r_attrib_poke_dim(ptype, shape);
 
   FREE(2);
-  return out;
+  return ptype;
 }
+
+// -----------------------------------------------------------------------------
 
 r_obj* ffi_vec_shape2(r_obj* x, r_obj* y, r_obj* frame) {
   struct r_lazy x_arg_ = { .x = syms.x_arg, .env = frame };
@@ -60,26 +46,70 @@ r_obj* ffi_vec_shape2(r_obj* x, r_obj* y, r_obj* frame) {
   return vec_shape2(x, y, &x_arg, &y_arg);
 }
 
+static inline
+r_obj* vec_shape2(
+  r_obj* x,
+  r_obj* y,
+  struct vctrs_arg* p_x_arg,
+  struct vctrs_arg* p_y_arg
+) {
+  // Expect that `r_dim()` does not allocate, so we don't protect these!
+  r_obj* x_dimensions = r_dim(x);
+  r_obj* y_dimensions = r_dim(y);
+
+  if (x_dimensions == r_null) {
+    if (y_dimensions == r_null) {
+      return r_null;
+    } else {
+      return dims_shape(y_dimensions);
+    }
+  } else {
+    if (y_dimensions == r_null) {
+      return dims_shape(x_dimensions);
+    } else {
+      return dims_shape2(x_dimensions, y_dimensions, x, y, p_x_arg, p_y_arg);
+    }
+  }
+}
+
+// -----------------------------------------------------------------------------
+
+// Sets the first axis to zero
+static inline
+r_obj* dims_shape(r_obj* dimensions) {
+  if (r_length(dimensions) == 0) {
+    r_stop_internal("`dimensions` must have length.");
+  }
+  if (r_typeof(dimensions) != R_TYPE_integer) {
+    r_stop_internal("`dimensions` must be an integer vector.");
+  }
+
+  if (r_int_get(dimensions, 0) == 0) {
+    // Already a shape, no clone required
+    return dimensions;
+  }
+
+  dimensions = KEEP(r_clone_referenced(dimensions));
+  r_int_begin(dimensions)[0] = 0;
+  FREE(1);
+  return dimensions;
+}
+
+// -----------------------------------------------------------------------------
 
 /*
- * Returns `NULL` if `x` and `y` are atomic.
- * Otherwise returns a dimensions vector where the first dimension length
- * is forcibly set to 0, and the rest are the common shape of `x` and `y`.
+ * Returns a dimensions vector where the first dimension length is forcibly set
+ * to 0, and the rest are the common shape of `x` and `y`.
  */
-static
-r_obj* vec_shape2_impl(r_obj* x_dimensions,
-                       r_obj* y_dimensions,
-                       r_obj* x,
-                       r_obj* y,
-                       struct vctrs_arg* p_x_arg,
-                       struct vctrs_arg* p_y_arg) {
-  if (x_dimensions == r_null) {
-    return vec_shape(y_dimensions);
-  }
-  if (y_dimensions == r_null) {
-    return vec_shape(x_dimensions);
-  }
-
+static inline
+r_obj* dims_shape2(
+  r_obj* x_dimensions,
+  r_obj* y_dimensions,
+  r_obj* x,
+  r_obj* y,
+  struct vctrs_arg* p_x_arg,
+  struct vctrs_arg* p_y_arg
+) {
   r_ssize x_dimensionality = r_length(x_dimensions);
   r_ssize y_dimensionality = r_length(y_dimensions);
 
@@ -120,7 +150,7 @@ r_obj* vec_shape2_impl(r_obj* x_dimensions,
     const int x_dimension = p_x_dimensions[i];
     const int y_dimension = p_y_dimensions[i];
 
-    p_out[i] = vec_dimension2(x_dimension, y_dimension, axis, x, y, p_x_arg, p_y_arg);
+    p_out[i] = dim2(x_dimension, y_dimension, axis, x, y, p_x_arg, p_y_arg);
   }
 
   for (; i < max_dimensionality; ++i) {
@@ -131,40 +161,16 @@ r_obj* vec_shape2_impl(r_obj* x_dimensions,
   return out;
 }
 
-
-// -----------------------------------------------------------------------------
-
-// Sets the first axis to zero
-static
-r_obj* vec_shape(r_obj* dimensions) {
-  if (dimensions == r_null) {
-    return r_null;
-  }
-
-  dimensions = KEEP(r_clone_referenced(dimensions));
-
-  if (r_length(dimensions) == 0) {
-    r_stop_internal("`dimensions` must have length.");
-  }
-
-  if (r_typeof(dimensions) != R_TYPE_integer) {
-    r_stop_internal("`dimensions` must be an integer vector.");
-  }
-
-  r_int_begin(dimensions)[0] = 0;
-
-  FREE(1);
-  return dimensions;
-}
-
 static inline
-int vec_dimension2(int x_dimension,
-                   int y_dimension,
-                   int axis,
-                   r_obj* x,
-                   r_obj* y,
-                   struct vctrs_arg* p_x_arg,
-                   struct vctrs_arg* p_y_arg) {
+int dim2(
+  int x_dimension,
+  int y_dimension,
+  int axis,
+  r_obj* x,
+  r_obj* y,
+  struct vctrs_arg* p_x_arg,
+  struct vctrs_arg* p_y_arg
+) {
   if (x_dimension == y_dimension) {
     return x_dimension;
   } else if (x_dimension == 1) {
@@ -175,7 +181,6 @@ int vec_dimension2(int x_dimension,
     stop_incompatible_shape(x, y, x_dimension, y_dimension, axis, p_x_arg, p_y_arg);
   }
 }
-
 
 // -----------------------------------------------------------------------------
 
@@ -189,12 +194,20 @@ r_obj* vec_shape_broadcast(r_obj* out, const struct cast_opts* p_opts) {
   r_attrib_poke_dim(out, r_dim(p_opts->x));
   r_attrib_poke_dim_names(out, r_dim_names(p_opts->x));
 
-  out = vctrs_eval_mask5(r_sym("shape_broadcast"),
-                         r_syms.x, out,
-                         r_sym("to"), p_opts->to,
-                         syms.x_arg, r_x_arg,
-                         syms.to_arg, r_to_arg,
-                         r_syms.call, call);
+  out = vctrs_eval_mask5(
+    r_sym("shape_broadcast"),
+    r_syms.x,
+    out,
+    r_sym("to"),
+    p_opts->to,
+    syms.x_arg,
+    r_x_arg,
+    syms.to_arg,
+    r_to_arg,
+    r_syms.call,
+    call
+  );
+
   FREE(4);
   return out;
 }
