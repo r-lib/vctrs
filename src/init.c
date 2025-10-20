3d8ed483fafb989eeeb79946549d238e9037e5bb
Davis Vaughan <davis@rstudio.com>
2025-10-20 12:20:04 -0400

    Make `vec_ptype_common()` use `list2()` (#2071)

--- src/init.c
+++ src/init.c
@@ -391,8 +391,8 @@ extern r_obj* ffi_new_data_frame(r_obj*);
 
 static
 const R_ExternalMethodDef ExtEntries[] = {
-  {"ffi_ptype_common",                 (DL_FUNC) &ffi_ptype_common, 1},
-  {"ffi_ptype_common_opts",            (DL_FUNC) &ffi_ptype_common_opts, 2},
+  {"ffi_ptype_common",                 (DL_FUNC) &ffi_ptype_common, 2},
+  {"ffi_ptype_common_opts",            (DL_FUNC) &ffi_ptype_common_opts, 3},
   {"ffi_size_common",                  (DL_FUNC) &ffi_size_common, 3},
   {"ffi_recycle_common",               (DL_FUNC) &ffi_recycle_common, 2},
   {"ffi_cast_common",                  (DL_FUNC) &ffi_cast_common, 1},
