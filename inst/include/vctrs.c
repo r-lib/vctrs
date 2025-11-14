73b565d9555614d9ea4ff949a4404d19d451ffca
Davis Vaughan <davis@posit.co>
2025-11-14 08:26:07 -0500

    Remove old `vec_is_vector()` callable
    It has been a few years since we deprecated it in favor of `obj_is_vector()`. Only dplyr uses the `obj_is_vector()` callable.
    
    No one using `vec_is_vector()` now:
    https://github.com/search?q=org%3Acran+vec_is_vector+language%3Ac&type=code
    https://github.com/search?q=org%3Acran+vec_is_vector+language%3AC%2B%2B+&type=code

--- inst/include/vctrs.c
+++ inst/include/vctrs.c
@@ -5,13 +5,8 @@ bool (*obj_is_vector)(SEXP) = NULL;
 R_len_t (*short_vec_size)(SEXP) = NULL;
 SEXP (*short_vec_recycle)(SEXP, R_len_t) = NULL;
 
-// Deprecated
-bool (*vec_is_vector)(SEXP) = NULL;
-
 void vctrs_init_api(void) {
   obj_is_vector = (bool (*)(SEXP)) R_GetCCallable("vctrs", "obj_is_vector");
   short_vec_size = (R_len_t (*)(SEXP)) R_GetCCallable("vctrs", "short_vec_size");
   short_vec_recycle = (SEXP (*)(SEXP, R_len_t)) R_GetCCallable("vctrs", "short_vec_recycle");
-
-  vec_is_vector = (bool (*)(SEXP)) R_GetCCallable("vctrs", "vec_is_vector");
 }
--- inst/include/vctrs.h
+++ inst/include/vctrs.h
@@ -10,11 +10,6 @@ extern bool (*obj_is_vector)(SEXP);
 extern R_len_t (*short_vec_size)(SEXP);
 extern SEXP (*short_vec_recycle)(SEXP, R_len_t);
 
-// Deprecated in favor of `obj_is_vector()`
-// version: 0.5.3
-// date: 2023-02-15
-extern bool (*vec_is_vector)(SEXP);
-
 void vctrs_init_api(void);
 
 #endif
