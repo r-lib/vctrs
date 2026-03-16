dfce1e9e5a84ea89656ad0095f537a0b3f8e1fac
Davis Vaughan <davis@posit.co>
2026-03-16 10:08:35 -0400

    Avoid `ATTRIB()` pointer comparison with compact seq
    Using inheritance check instead

--- src/utils.c
+++ src/utils.c
@@ -745,7 +745,8 @@ r_obj* list_pluck(r_obj* xs, r_ssize i) {
 
 
 // Initialised at load time
-SEXP compact_seq_attrib = NULL;
+const char* compact_seq_class_string = NULL;
+SEXP compact_seq_classes = NULL;
 
 // p[0] = Start value
 // p[1] = Sequence size. Always >= 0.
@@ -789,14 +790,14 @@ SEXP compact_seq(R_len_t start, R_len_t size, bool increasing) {
   int* p = INTEGER(info);
   init_compact_seq(p, start, size, increasing);
 
-  SET_ATTRIB(info, compact_seq_attrib);
+  r_attrib_poke_class(info, compact_seq_classes);
 
   UNPROTECT(1);
   return info;
 }
 
 bool is_compact_seq(SEXP x) {
-  return ATTRIB(x) == compact_seq_attrib;
+  return r_inherits(x, compact_seq_class_string);
 }
 
 // Materialize a 1-based sequence
@@ -2010,9 +2011,10 @@ void vctrs_init_utils(SEXP ns) {
   fns_as_data_frame2 = r_env_get(ns, syms_as_data_frame2);
   fns_colnames = r_env_get(R_BaseEnv, syms_colnames);
 
-  compact_seq_attrib = Rf_cons(R_NilValue, R_NilValue);
-  R_PreserveObject(compact_seq_attrib);
-  SET_TAG(compact_seq_attrib, Rf_install("vctrs_compact_seq"));
+  compact_seq_class_string = "vctrs_compact_seq";
+  compact_seq_classes = Rf_allocVector(STRSXP, 1);
+  R_PreserveObject(compact_seq_classes);
+  SET_STRING_ELT(compact_seq_classes, 0, Rf_mkChar(compact_seq_class_string));
 
   compact_rep_attrib = Rf_cons(R_NilValue, R_NilValue);
   R_PreserveObject(compact_rep_attrib);
